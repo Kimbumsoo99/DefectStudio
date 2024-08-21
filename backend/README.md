91d1b533f438a61f02c9095488ce930ed794bd12
garamgim <garamgim@gmail.com>
2024-08-21 17:48:40 +0900

    Chore: 초기 프로젝트 설정

---
+++ backend/README.md
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6a6552cce315979274c3fded300a09c9ae6c3217abd803a682b3b8658f85d62b
+size 604
---
+++ backend/main.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:67351baeec5a8acf22e36ff66dbb58150dac2b0b03ca63ee05b2d2fe8f900f61
+size 497
