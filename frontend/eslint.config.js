c4422df78375dad6b883e4a4317cb9db315385cf
surina125 <jhyunsoo1125@gmail.com>
2024-08-26 11:49:36 +0900

    Chore: 초기 Config 및  프로젝트 구조 생성

---
+++ frontend/eslint.config.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b530f979ed45330c2cabf7680d9f067453a620f019ee342393ffa28b35efec39
+size 982
---
+++ frontend/src/components/generation/Inpainting.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:eaa601f5cbbd9e0e0a9f4dc6c3b08c6cef407ea5fbfbed22658e6e7f6c2412c4
+size 96
