1ad340e867a79ca747dcfeec54328552802ff8e1
garamgim <garamgim@gmail.com>
2024-08-26 22:25:10 +0900

    [#6] 현재 로그인한 유저 반환 기능
    Former-commit-id: 5d0176473f6821423a1bb0c32f202be002ffd320

--- backend/dependencies.py
+++ backend/dependencies.py
@@ -1,8 +1,3 @@
-from core.db import Session
-
-def get_db():
-    db = Session()
-    try:
-        yield db
-    finally:
-        db.close()
+version https://git-lfs.github.com/spec/v1
+oid sha256:01c99cd3be41618df7a7a19bb419a0e4249d37720a32c1a3d17ca3ed10d78365
+size 1482
