e487b08a108b6bbed9afa749cac5328b45dbf217
garamgim <garamgim@gmail.com>
2024-08-27 01:02:19 +0900

    [#6] 리프레시 토큰 검증 기능 #in-progress
    Former-commit-id: 0d7619ca7174d7e67667a9a6c863bbc125b723ee

--- backend/core/config.py
+++ backend/core/config.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7fc8bbdc313c0a5ade930d0163b2f2c3d4117c939f58bed1499974e805abb66b
-size 1427
+oid sha256:62f4a75b1c42ecf72ca97e7289f63b310328820aa84a9a4e8d21b591c14e6872
+size 1510
--- backend/dependencies.py
+++ backend/dependencies.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:01c99cd3be41618df7a7a19bb419a0e4249d37720a32c1a3d17ca3ed10d78365
-size 1482
+oid sha256:fbf806cfe33e7f2f44e7d8e51eeaa92618f63e85867bc18add4e3acc268fb3e2
+size 1741
