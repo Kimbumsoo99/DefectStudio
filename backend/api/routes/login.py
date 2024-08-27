6558a70360155b9e8a2d3f7b181c6dddd46cd71a
garamgim <garamgim@gmail.com>
2024-08-28 00:57:04 +0900

    [#6] feature: 로그아웃 API
    Former-commit-id: 572be67d19b0380fb270a8334b40427cd548c87c

--- backend/api/routes/login.py
+++ backend/api/routes/login.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:72106d5dbcc4d1fbbfc35d911037dbc58e4b225c5ea66bc2c442ff98af33d7e1
-size 2458
+oid sha256:1b7b34e1e04bd05bf1248b19e762c329a2d2e60e8474333ab21ef41d23979b43
+size 3361
