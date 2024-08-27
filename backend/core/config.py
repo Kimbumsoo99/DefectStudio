87fc9d36e7e8f802e9203a9dc586d670f5715634
garamgim <garamgim@gmail.com>
2024-08-27 15:29:52 +0900

    [#6] chore: import문 정리
    Former-commit-id: bd27bce8798eee876cafe65373b2912efe4c0db7

--- backend/core/config.py
+++ backend/core/config.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7211a20108d5d3ca90201e3781935fc965d2feabea040cb597df04c19903acf8
-size 1552
+oid sha256:59207e624a1be1c6e64b97e5179e27ac7008ba0d7d667ea1762d09d887ddb0da
+size 1554
