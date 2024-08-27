00ce50817e78206671022acdda70826c0c52b540
garamgim <garamgim@gmail.com>
2024-08-27 11:54:33 +0900

    [#6] chore: 에러 메시지 수정
    Former-commit-id: 8e1f71c194673824e5c1e445704166af001db7e6

--- backend/dependencies.py
+++ backend/dependencies.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1e99ab0d0dbf2b7cd954d965322a1d86b68bf9b59080d0520a657ceb364ed4a7
-size 1770
+oid sha256:83aa9211d0f4f18fcb10e4f6f96197c909e4689d241b6d7699e739e9a8c15c90
+size 2007
