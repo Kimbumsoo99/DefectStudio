09f5dce8485e7901a686ce0bb57137524719894c
박수진 <tnwlssla20@naver.com>
2024-08-27 15:40:23 +0900

    [#11] refactor: 회원가입 DTO 유효성 검사 적용
    Former-commit-id: bed881385c2a24ebfee8cda56990fe62442fae42

--- backend/schema.py
+++ backend/schema.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eb81b3b2424dc349b255d9a61c61ea407985a638ae0ad89074ad98a96388d92f
-size 245
+oid sha256:23f517ffa9956204152b7ab38d8cfd8f85a12f6653d67e7d2ee4942e216398c9
+size 479
