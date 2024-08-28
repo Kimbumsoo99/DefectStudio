44a6278b199741827e0c937d1468f79df1e6ee9b
SnowySparks <originpc@naver.com>
2024-08-28 09:11:49 +0900

    [#5] fix : navBar 드롭다운 가리는 문제 해결
    Former-commit-id: d09870fcf1c2da93926864aaf95f0a3b5c561b21

--- frontend/src/components/common/Navbar.tsx
+++ frontend/src/components/common/Navbar.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:22b36fd4338e52ef2ed379e9b9ee9df705f79954ad395a2f008b2f7bbb5d8c26
-size 4835
+oid sha256:b6c062333f2c8030e50458c7b67581f7164d8bdafe2756130c737b4cef087b31
+size 4838
--- frontend/src/components/home/AIToolBoxItem.tsx
+++ frontend/src/components/home/AIToolBoxItem.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d3bd2c4f6bbfc79ed82c5bc53662672b62a48cdc567f628778514ef1d0356bee
-size 638
+oid sha256:cf7343fe653559875eac17d7321a4f6887d20df35adb00fef1387a3c5ed663ca
+size 634
