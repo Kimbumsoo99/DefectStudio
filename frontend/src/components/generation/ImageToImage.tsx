71f34d996e37d04e7406e63cd282b4c29e6d1936
김경대 <originpc@naver.com>
2024-08-28 10:46:03 +0900

    Revert "Merge branch '5-feature' into 'FE'"
    This reverts merge request !10
    
    Former-commit-id: 1fc3c189b381f92163ba471c1fd2ed027f022d86

---
+++ frontend/src/components/generation/ImageToImage.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:aa990d21600e4278ecf85ef7539c417833462a621d3238940486472e51996a0a
+size 87
---
+++ frontend/src/components/generation/RemoveBackground.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5fff7be66eedf48b82de965ac438baf9186e655221f5040f167dfb446d70e0c9
+size 114
