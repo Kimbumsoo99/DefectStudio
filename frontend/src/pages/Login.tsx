e61a10aaca1844d5d64fcec025fdab84b3b8f6e2
SnowySparks <originpc@naver.com>
2024-08-28 14:53:32 +0900

    [#12] ui : 로그인 홈 페이지 ui수정 및 왼쪽부분 화면 추가
    Former-commit-id: 4fe570473b39a44aad61a6176e37c2918268f3d0

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e03a8dbaf507f1d8924527aee0fa91f3fed14f4b42e9def1788f2b23ece606ec
-size 2916
+oid sha256:9d8f4a7da2f323e853703356365f3f40c9c9a9395d1944e2dd16cef36649a69d
+size 3174
