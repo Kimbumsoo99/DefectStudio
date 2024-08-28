1a854b12d168d2fb8ab0e7341882291f9344a731
SnowySparks <originpc@naver.com>
2024-08-28 16:20:49 +0900

    [#12] refactor: 컴포넌트 분리 및 함수시행부분 작성
    Former-commit-id: e2a328c31c07030ee6cb6bdf8876cb786302ea02

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c4f50f7e038ef82cfb0ab2aa8da4f3cfc9cdd5e13c08b5e2ba57174a14a352b5
-size 3097
+oid sha256:f50723b8043b1431a6267c25edbd65aedefb5ab8d21d0d48b366e34db86dbb54
+size 1555
