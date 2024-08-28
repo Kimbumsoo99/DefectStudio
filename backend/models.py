b56d50505703affacf614ef0d5c3599076006fc0
garamgim <garamgim@gmail.com>
2024-08-28 09:07:08 +0900

    [#6] chore: login_id unique 제약조건 추가
    Former-commit-id: 0b19587143349ed0a2d03dadfea5b482195c9a5c

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f0330d70a5ea0d5335b147d5ef506713a6e48750dc1e10b11920ef1e20cc5c21
-size 983
+oid sha256:d6763763a4a2b2dfa1199dca1de486c30cd774055e37267fa5fcdfe0d75ec578
+size 996
