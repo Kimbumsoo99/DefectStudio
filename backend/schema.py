284a4f9588e221a494200f4e9076da638ee5fb32
garamgim <garamgim@gmail.com>
2024-08-28 17:51:53 +0900

    [#15] feature: 회원 조회 기능 #in-progress
    Former-commit-id: 5baa826e57464deb330af7aacccc18bbe2984ff2

--- backend/schema.py
+++ backend/schema.py
@@ -1,12 +1,3 @@
-from pydantic import BaseModel, EmailStr, Field
-from enums import Role
-import re
-
-class MemberCreate(BaseModel):
-    login_id: str = Field(..., min_length=3, max_length=50)
-    password: str = Field(pattern=re.compile(r"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,20}$"))
-    name: str = Field(..., min_length=3, max_length=100)
-    nickname: str = Field(..., min_length=3, max_length=50, )
-    email: EmailStr
-    role: Role = Field(default=Role.department_member)
-    department_id: int = Field(...)
+version https://git-lfs.github.com/spec/v1
+oid sha256:d0fa794ba175c9faeb99068ebfde6b7d55169248647c66c499063b169a33e87a
+size 1050
