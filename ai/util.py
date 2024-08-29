104b5975c6556585e592512a95efd1658558839a
garamgim <garamgim@gmail.com>
2024-08-30 01:20:40 +0900

    [#18] feature: 이미지 한장 및 여러장  S3 업로드 기능 구현
    Former-commit-id: afa389ad0f46fddf95629ab4b458ac424acc8862

--- ai/util.py
+++ ai/util.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3f38b56ed77ba4ddc3b2f96aa88364ccc73ddffc13d53e701ab62decbb0a10a7
-size 184
+oid sha256:09d960263521cfd3f699e191e294230100685e5fab54093308f6eebcde982560
+size 1285
