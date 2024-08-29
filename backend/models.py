fbe3629b9082efb48cad67f33a557af5926ab73c
박수진 <tnwlssla20@naver.com>
2024-08-29 13:47:49 +0900

    [#16] feature: 토큰 model 구현
    Former-commit-id: b54b7916d3c9e7e4872548840ce58f2d289270d2

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:83509d66b3b8f9feb111cdeef58ed94c2013efeaf2b5d6d78218498f847b62e0
-size 1007
+oid sha256:4320cfab06eef2353d080fc8c089db0c1192a666cc01f97ff67c4c2115855959
+size 1539
