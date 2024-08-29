5106598ebece4d36c6c08fe3d95499e02c3e73dd
SnowySparks <originpc@naver.com>
2024-08-29 15:09:16 +0900

    [#17]fix : ERD에 맞는 회원가입 정보로수정 #S11P21S001-92
    Former-commit-id: 4f08adcbf614d6014d24a09f0cb75a8bac920aae

--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0bf978cc9e08e5e7059a8b56e00f4d12acd5e7395166e848f84492f9ffa83132
-size 2894
+oid sha256:6c4e78ee828be6af4facca3cee3dbd83ccc609e04a2b5f7053712e540dbad907
+size 3428
