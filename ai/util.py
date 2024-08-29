002dabc911fbadbadd3c720b7039b63c3c86bee1
garamgim <garamgim@gmail.com>
2024-08-30 01:27:29 +0900

    [#18] feature: 이미지 한장 및 여러장 S3 삭제 기능 구현
    Former-commit-id: 7966941c4f7e4093f9cb36cb45c45cec6ce951f4

--- ai/util.py
+++ ai/util.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:09d960263521cfd3f699e191e294230100685e5fab54093308f6eebcde982560
-size 1285
+oid sha256:da9adbca69e2803043b69f29c4bded92096bf594cea1e815e453ffba8a864994
+size 1837
