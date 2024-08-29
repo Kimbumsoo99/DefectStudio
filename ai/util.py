29cc378cd92705200d7d38562de97d81ee845feb
garamgim <garamgim@gmail.com>
2024-08-30 01:32:29 +0900

    [#18] chore: 전역 s3 client 삭제
    Former-commit-id: 9fbb2e74399b5a7c5b4a0210e95684e865497a8d

--- ai/util.py
+++ ai/util.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:da9adbca69e2803043b69f29c4bded92096bf594cea1e815e453ffba8a864994
-size 1837
+oid sha256:d26e455648dd5e1ee8f6d0930ea86f8a2c94d8e1c80d314d45c945aed0d6b536
+size 1691
