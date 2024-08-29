05ec517723885504cb4a69f02ad5431946bed17b
박수진 <tnwlssla20@naver.com>
2024-08-29 14:24:45 +0900

    [#16] refactor: role decorator 반환값 수정
    Former-commit-id: 2149102dca8b2b3159575081a2c75f15dd4723d8

--- backend/api/routes/admin.py
+++ backend/api/routes/admin.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a1ebcfcde2e45e139bdc177b53489f269be725a90d79fdb9b32401562357204a
-size 1119
+oid sha256:6f91965eb16add5129f474b3c2ec2874f31a544a317e00165e2e84bad3404b13
+size 1265
