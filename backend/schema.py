f52e92c1fb6b177da771c7a1ec8f7c748f543fd8
박수진 <tnwlssla20@naver.com>
2024-08-29 13:56:58 +0900

    MERGED: conflict solved and merge feature/be/16-token-create into be
    Former-commit-id: 6c904f4b987ec1f89e894f330f029b76def232ec

--- backend/schema.py
+++ backend/schema.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1b4c193203d8e3637baed379e9bf7df6d10cd877fa41cd352d3df15e9fe0d804
-size 1244
+oid sha256:4b7e90352aa42665642ee270774ba3befe4f7372bf2a68e5b19adc500abce765
+size 1416
