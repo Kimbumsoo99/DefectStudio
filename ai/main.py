47bf6ded7263f6f371d6a49a06e35b99ee0edba3
garamgim <garamgim@gmail.com>
2024-08-30 11:16:14 +0900

    [#18] chore: key 오류 수정 및 delete 테스트 메소드 추가
     #S11P21S001-78 #done
    
    
    Former-commit-id: 71a33f329e5bc1381c888fe21951ae618fe7bab3

--- ai/main.py
+++ ai/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:399fa8f549a4691b4e9088a4c66a4a6a603c978dc8546974cd3f71e3db637daa
-size 459
+oid sha256:d76db326ab1d64b99cdd552a739abf84b74a9629a7aad2be72917077bd36fc72
+size 723
