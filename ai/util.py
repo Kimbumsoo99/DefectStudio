f7dfd2a5fb61a2cb5094d5f4d76ed0720d0c4cd6
garamgim <garamgim@gmail.com>
2024-08-30 10:52:26 +0900

    [#18] feature: aioboto3에서 boto3으로 변경 및 main에 테스트 메소드 생성
     #S11P21S001-78 #in-progress
    
    
    Former-commit-id: 266e5c661c9a77d0518bc005cad12e9c4e32c14c

--- ai/util.py
+++ ai/util.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e4afe3198ce72d1cf89908a67cadc30e24416040ad956244154ec241e112c9c0
-size 1714
+oid sha256:39b6540b8bc746d13459fa93544110199f0e295ae268fdc1c6ea66bf0ea120b1
+size 1777
