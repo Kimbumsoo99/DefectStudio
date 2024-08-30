7e18d1c5b8886577d53118a1d78fe1064bca7edb
박수진 <tnwlssla20@naver.com>
2024-08-30 13:20:51 +0900

    [#19] feature: TokenUsageCreate DTO 구현
    #S11P21S001-59
    
    
    Former-commit-id: 64e798dbf7ff4a026ba3eca02db121ecd30bcd43

--- backend/schema.py
+++ backend/schema.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4b7e90352aa42665642ee270774ba3befe4f7372bf2a68e5b19adc500abce765
-size 1416
+oid sha256:303391d6274b7bf8a010012c8e466a5e57c44f404ec6d9b964a208b0bf8f0e8d
+size 1532
