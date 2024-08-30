2ea28a62eed7645dae56490fee8d08b8e6998097
garamgim <garamgim@gmail.com>
2024-08-30 09:29:27 +0900

    [#18] chore: S3 URL 변경
    Former-commit-id: 17da5014cc041dbacc61fabcd6795d7b4cc77fec

--- ai/config.py
+++ ai/config.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d51528201cf694e3a5253c555fce397fb6edd6421ecda7c40f0ed0d604231d24
-size 315
+oid sha256:49a3169b511e704131f47334f3ba918d648b10cb873f894b3fc999ce0068bc7f
+size 368
