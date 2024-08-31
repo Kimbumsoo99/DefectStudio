8865acac92eee4daa11bcb7206422bf197bbf681
박수진 <tnwlssla20@naver.com>
2024-08-31 22:39:53 +0900

    [#26] feature: TokenUsageRead DTO 구현
    Former-commit-id: 8fd33be403eb4ffec7e3442ebaecb62c4af556ea

--- backend/schema.py
+++ backend/schema.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:23f1cf253a72441b994e0dfbfca363ccf16a3ad36cbb37bf1b8d85dc7f2abdff
-size 2247
+oid sha256:ef152d6186604af443501bb52b68c6d72d4d1daafc00f986ca0645aa58486909
+size 2662
