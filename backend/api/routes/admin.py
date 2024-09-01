d2902b48034bf6b9ef96b79e0ef8404eb10f01a7
박수진 <tnwlssla20@naver.com>
2024-09-01 18:16:27 +0900

    [#28] refactor: 반환값 변경
    Former-commit-id: a1d415ce5ded88e19d61d816f71b774243fc9907

--- backend/api/routes/admin.py
+++ backend/api/routes/admin.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a59cbd9175af901ac810c36260df3db6ebba3f0e663ff2f08f4cb4ac7d760195
-size 3967
+oid sha256:48686d941ed6004f9ad42df416f9ecde51973f39b05bf87d0ef6b6c71aa02475
+size 3969
