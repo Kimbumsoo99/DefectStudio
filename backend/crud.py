ee1248ca0c859cea49aff87a63fe72163dd284fb
박수진 <tnwlssla20@naver.com>
2024-09-01 18:17:50 +0900

    [#28] feature: 토큰 사용 관련 crud 메서드 구현
    Former-commit-id: 099c04de3d65bea8e4a316f0ea2d9fbd1a9c67da

--- backend/crud.py
+++ backend/crud.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1466c48d2d7618d6bce4b5729cd8eb60af00a1f810e17cc4394aa4989cb3cf24
-size 4051
+oid sha256:256231ffa1e07a064843af5e46008ff93047125b277b6be4e1bbf43b1f4b40e8
+size 4865
