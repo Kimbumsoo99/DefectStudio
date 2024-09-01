737eaadadf4dd46e5a608b2561705df703b26a76
박수진 <tnwlssla20@naver.com>
2024-09-01 23:30:34 +0900

    [#31] feature: 토큰 발급/분배 로그 추가 구현
    Former-commit-id: f0d16cfa5bd80e523c93fd3bdd763e3707f169e3

--- backend/api/routes/admin.py
+++ backend/api/routes/admin.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:48686d941ed6004f9ad42df416f9ecde51973f39b05bf87d0ef6b6c71aa02475
-size 3969
+oid sha256:5545e900e8ff3cf68a5076a2e0905968ae12610ceb1058faefd06ab0ab409262
+size 4344
