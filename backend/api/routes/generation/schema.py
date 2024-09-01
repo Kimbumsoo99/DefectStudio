a8c848d661705fe204a4f61e19d2cf68b4d23244
garamgim <garamgim@gmail.com>
2024-09-01 19:01:02 +0900

    [#24] chore: TTIRequest 파라미터 추가
    Former-commit-id: 71ad6c358297a2cc0798788079aec331d082d9e2

--- backend/api/routes/generation/schema.py
+++ backend/api/routes/generation/schema.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:855a7f310fc887f2a943e6195b9cbfb0beb151b2c20e9c854465351672a50013
-size 498
+oid sha256:5b2718b0eed65b5678e346d5d68220ffd48b31168ce3a1fbe9f2f495ae8c7a3a
+size 550
