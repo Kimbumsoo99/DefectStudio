79581ff3edd1a6dce039991b704ff9fc6bcbf7f3
박수진 <tnwlssla20@naver.com>
2024-09-01 15:42:10 +0900

    [#27] feature: 토큰 만료 위한 crud 메서드 추가
    Former-commit-id: c0d1129c8ef54c039b6101671002aa540061e1a4

--- backend/crud.py
+++ backend/crud.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1dbf8103b5ab04909106b069c5594b4d2a79f5443a6d16a056e850b23e11765d
-size 3511
+oid sha256:1466c48d2d7618d6bce4b5729cd8eb60af00a1f810e17cc4394aa4989cb3cf24
+size 4051
