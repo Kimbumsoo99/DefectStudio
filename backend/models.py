cf9e13580d6b6a566d5a31ca7c0e965a8cf09c21
박수진 <tnwlssla20@naver.com>
2024-09-01 15:41:53 +0900

    [#27] feature: Token 복합인덱스 추가(end_date, is_active)
    Former-commit-id: 9626ddde9075959f0696d04cb9544089123ee023

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7e955514d4145bbe8cc647e5e78f93a017557c87f1060f14dad73fe73a66ea27
-size 2338
+oid sha256:f1813390b459b66f153597dcee143feb696fae6cfa37e3b2d8145c8c656c25f7
+size 2418
