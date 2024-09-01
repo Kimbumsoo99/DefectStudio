0cff4300acdd5f6a88e26633782f2e06411fccea
garamgim <garamgim@gmail.com>
2024-09-01 18:29:01 +0900

    [#24] feature: TTI 요청 API
    Former-commit-id: 562cc787e1e10eb35f01f697c652378413a1e653

--- backend/core/config.py
+++ backend/core/config.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:59207e624a1be1c6e64b97e5179e27ac7008ba0d7d667ea1762d09d887ddb0da
-size 1554
+oid sha256:8cb2dadaa0f8bcb525d980dca61ce022b9ff0e08138d39ea8536e448c31ea779
+size 1585
