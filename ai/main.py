07cad2444caa17c93438f776d1dd52f22759d7d9
garamgim <garamgim@gmail.com>
2024-09-01 18:58:09 +0900

    [#24] feature: TTI 생성 API
    Former-commit-id: fe21a43acae67de1ae3c0a2e7a3ff267f7a141d3

--- ai/main.py
+++ ai/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d76db326ab1d64b99cdd552a739abf84b74a9629a7aad2be72917077bd36fc72
-size 723
+oid sha256:f6d25e0908cf6ed9d21f7f89069057c9e46c1ea4793255258fdd4e72008dc4af
+size 803
