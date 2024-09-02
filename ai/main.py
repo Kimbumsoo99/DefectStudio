ee9d9a8235668f1da5ba683f78ba2781e3248682
garamgim <garamgim@gmail.com>
2024-09-02 22:36:14 +0900

    [#24] chore: 코드 포맷팅 및 불필요한 임포트문 제거  #S11P21S001-161 #in-progress
    Former-commit-id: ffe95c8468c6ff3284167a6f71051a4adc8c7f77

--- ai/main.py
+++ ai/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:73c8ab72346692ddd419a7cbf0432cafcd41c3686754a23ef7ab068ac2ff407f
-size 244
+oid sha256:24918a755f1075631d4665eaf6c2f778f6c2a3c6951c6ad1d4f7a26d38b79b78
+size 222
