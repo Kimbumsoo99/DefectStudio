1b4936eaf79b0773566474ee80da60077366a5a8
garamgim <garamgim@gmail.com>
2024-09-02 22:31:36 +0900

    [#24] feature: GPU 환경에 따른 백엔드 서버 Response 분기  #S11P21S001-161 #in-progress
    Former-commit-id: 44d23ba823465303d7ca9d9ff5afcd2eb5dc3af8

--- backend/enums.py
+++ backend/enums.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ffe1887969d6c568e8d05db7da0b47acebcb2aff326d255a252cb847fe3c2cca
-size 463
+oid sha256:44bb7c4d7b08237a2b305c76bc20ba6dab22319d0ce4bd16e684b6cef65339cb
+size 539
