43629d9f4a42f64e1ae8e4ce29b1d268d0d23f67
garamgim <garamgim@gmail.com>
2024-09-02 17:51:03 +0900

    [#37] chore: AI 서버에서 S3 기능 삭제
    Former-commit-id: 6f3ecf480f58b314efe9546a114068a09c58c6af

--- ai/main.py
+++ ai/main.py
@@ -1,3 +1,7 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:d76db326ab1d64b99cdd552a739abf84b74a9629a7aad2be72917077bd36fc72
-size 723
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="localhost", port=8001)
