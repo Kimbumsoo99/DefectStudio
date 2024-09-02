637e3d0f9b2e4198bce473887af676748d8d9709
garamgim <garamgim@gmail.com>
2024-09-02 22:35:27 +0900

    [#24] AI 서버 API Endpoint 정리  #S11P21S001-161 #in-progress
    Former-commit-id: 594a076283696331d8dc7e7ca902067f47fc66b6

--- ai/api/main.py
+++ ai/api/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b3d0aaceaf2b2bc379cf38a21e0fed017aff3b23f4c84aa6cc759acac7db8dd6
-size 119
+oid sha256:bb9998f6cc3d25c78cc85df82787bb7d1c1614f91c105ab66c08daaad8838ac1
+size 174
--- ai/main.py
+++ ai/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:61f2f444444dea8495865374ffe4f814c5b00c94840d3920f3fcd1f0b11daede
-size 138
+oid sha256:73c8ab72346692ddd419a7cbf0432cafcd41c3686754a23ef7ab068ac2ff407f
+size 244
