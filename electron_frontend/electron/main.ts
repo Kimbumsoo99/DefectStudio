82accb70408908d29b7db3d709e832c938becbed
SnowySparks <originpc@naver.com>
2024-09-02 15:22:57 +0900

    [#35] feature: 미니 프로필 및 프로필 화면 큰 틀 작업
    #S11P21S001-214 프로필 페이지 큰 틀 완성
    
    
    Former-commit-id: abefaf14b4a483159ee65b27172b6b5462bace4b

--- electron_frontend/electron/main.ts
+++ electron_frontend/electron/main.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b0d6e07ff3a2ef6787fd317b25bd79bfb5085cf9899b91e13fc5bfe3baa448fd
-size 2077
+oid sha256:1366e7ca0a03615faf0d504ee0e24d92772208bb8a95214e69fc3f5176e57e24
+size 2175
---
+++ electron_frontend/src/pages/Profile.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:53a3a226576c382cfa97cd72575eb7d30ad790e9423d94c47bab8c5e896d2476
+size 4164
