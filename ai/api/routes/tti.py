23ad28714286bff76954c107b6e4a653051c2c42
garamgim <garamgim@gmail.com>
2024-09-02 17:20:08 +0900

    [#24] feature: TTI 이미지 생성 API  #S11P21S001-161 #in-progress
    Former-commit-id: 45c3386abced174f94eaa1c92214b32e47761fe8

--- ai/api/routes/tti.py
+++ ai/api/routes/tti.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fbe9132baa54aca7d281335668246bd3eafba4beefd2d482ae4cf630f1f4b0b0
-size 2446
+oid sha256:86d1a3aefe76f33d732fc22f8fc34f9d37990aefbffcfac7d0c27c4c86f131e7
+size 2569
