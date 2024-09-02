9c52ea7a54352c831b23d830fa835fe4ac2667ac
surina125 <jhyunsoo1125@gmail.com>
2024-09-02 10:49:24 +0900

    [#30] feature: start training parameter 추가 중
    Former-commit-id: 42a73cdc1be3ba4955c86d789fa8b0270a6c8255

--- electron_frontend/src/components/generation/masking/InpaintingModal.tsx
+++ electron_frontend/src/components/generation/masking/InpaintingModal.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8691c7fa3507aac856bc241c4ce84752edd04caf2ab29a120a9867d1a573770b
-size 23485
+oid sha256:95b4cc2f585dac57c941f7725f674c0036e482cf861ebbf036bf1172c1783f9d
+size 26180
