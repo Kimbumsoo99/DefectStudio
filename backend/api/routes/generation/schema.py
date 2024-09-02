163e1b65ebc8f8a78f557986682fc812018f8ca6
garamgim <garamgim@gmail.com>
2024-09-02 10:59:18 +0900

    [#24] chore: model 변경  #S11P21S001-161 #in-progress
    Former-commit-id: f17ed514ca71f99da2319a89f748649cfe6938c8

--- backend/api/routes/generation/schema.py
+++ backend/api/routes/generation/schema.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1eb0a0d9961a52887619c101fc091a3fef89c3adf4432feb58c5eee49e5754b2
-size 579
+oid sha256:c826d4cb37122f30f15f12796f5ff7735d289c7f695136787e9b02b4386443e4
+size 578
