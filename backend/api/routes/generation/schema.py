a163adc62b6acb37bbc71c4c6be53962ed872430
garamgim <garamgim@gmail.com>
2024-09-02 22:31:36 +0900

    [#24] feature: GPU 환경에 따른 백엔드 서버 Response 분기  #S11P21S001-161 #in-progress
    Former-commit-id: 02c4f3d85ec9ad0229e41ed4ab4c285ad12eba7e

--- backend/api/routes/generation/schema.py
+++ backend/api/routes/generation/schema.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c826d4cb37122f30f15f12796f5ff7735d289c7f695136787e9b02b4386443e4
-size 578
+oid sha256:677e0e1cb8b6b7fdc60690c7e880f94bfbffa10c807d6c485f71ccc9f0f60215
+size 623
--- backend/api/routes/generation/tti.py
+++ backend/api/routes/generation/tti.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fc7c82ff3b741a02fb821b4c9a5df4725b2e72a5bd016f6525330d4761916124
-size 1383
+oid sha256:cae424b3b3286756b51a52375088a40ae91f28805dae00befb959eaee023e98e
+size 1780
