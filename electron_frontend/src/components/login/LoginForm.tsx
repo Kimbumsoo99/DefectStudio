530f793b869561b59bffdc250a883a8755b6688b
SnowySparks <originpc@naver.com>
2024-09-03 10:54:13 +0900

    [#36] fix: 로그인 에러 메세지 처리
    Former-commit-id: a84d9f79496e70e6e43bed6957c76c0661ba0139

--- electron_frontend/src/components/login/LoginForm.tsx
+++ electron_frontend/src/components/login/LoginForm.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a139c5683e233d0551df2ea14bebf1e317938506d7ae44210c5a3a5c8f8f4486
-size 1968
+oid sha256:ea9660f84c4552ee4bb5f87602c0b269815b8b058475cb0769e00c7e3596d7fd
+size 2266
