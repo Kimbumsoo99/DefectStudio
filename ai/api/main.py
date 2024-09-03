ca7a534733a869365b5d5280f155bbd8e1258464
박수진 <tnwlssla20@naver.com>
2024-09-03 11:07:55 +0900

    [#32] feature: AI rembg router 추가
    #S11P21S001-184
    
    
    Former-commit-id: 00b32a335af72c868d75116a38a2ff5a3c9d495c

--- ai/api/main.py
+++ ai/api/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bb9998f6cc3d25c78cc85df82787bb7d1c1614f91c105ab66c08daaad8838ac1
-size 174
+oid sha256:17f9ba098e1ecf89caaae12b269f6cd83de85707963116d679685781d25e0da7
+size 221
