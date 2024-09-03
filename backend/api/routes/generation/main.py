b4df2067eb2df9cd50ab910c6866e56ac27f88af
박수진 <tnwlssla20@naver.com>
2024-09-03 17:30:23 +0900

    [#39] feature: Cleanup Generation router 설정 BE
    #S11P21S001-187 #in-progress
    
    
    Former-commit-id: b3716c08d2001e0543abf4d4f24acbe0e75ec210

--- backend/api/routes/generation/main.py
+++ backend/api/routes/generation/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dd5bc67e965b99c6a80e1ae5b11e034be4cccfeb9c0a011a872bbc275f6a521c
-size 220
+oid sha256:53ac5f78bd418b563d10fe32eaa143dad79e4480020203b94d1977d91838254e
+size 267
