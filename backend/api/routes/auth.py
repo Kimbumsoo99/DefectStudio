83ab9c77d22a108b338cc26fd8d674e86bd52063
garamgim <garamgim@gmail.com>
2024-09-03 14:33:56 +0900

    [#38] chore: 로그인시 회원 권한 content로 리턴
    Former-commit-id: 780b4fc65e5a459cdca8bc62c6e1033b99f398d6

--- backend/api/routes/auth.py
+++ backend/api/routes/auth.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0b245b22ff6813f2b8d647f96be1331d464329ecadb39e753f7f59f2ea4b3346
-size 3373
+oid sha256:d376d2ff670ffdbc585a0c22e29fb82580754e4feb5d2562558899adf431182f
+size 3565
