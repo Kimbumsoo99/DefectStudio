845110d0eb385897d4b0db2d8c0138ade800810c
garamgim <garamgim@gmail.com>
2024-09-04 00:15:32 +0900

    [#41] chore: InpaintingRequest 모델 기본값 수정  #S11P21S001-166 #in-progress
    Former-commit-id: 32b5247d263e6e3063bbff899e5050c7c094a355

--- backend/api/routes/generation/schema.py
+++ backend/api/routes/generation/schema.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:69bfe7c513f0966132866889020699583bf95ed549cabe32478e7f87e8331193
-size 3948
+oid sha256:f9f539f39a60e575f5a92dd9bc605afa9f13bc15f9cc703abf90c7d59e558b6d
+size 3967
