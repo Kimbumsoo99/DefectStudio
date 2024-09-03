75ed164c2b1760337dee09b36fa38efd7003b77a
garamgim <garamgim@gmail.com>
2024-09-04 00:23:22 +0900

    [#40] chore: 변수명 수정 및 router 추가  #S11P21S001-167 #in-progress
    Former-commit-id: a3960d91c758058040ab58b30256494a1ad4a47d

--- ai/api/main.py
+++ ai/api/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:17f9ba098e1ecf89caaae12b269f6cd83de85707963116d679685781d25e0da7
-size 221
+oid sha256:985ed2df017190c84954af20c9ea56e265185e57ab7f7a4bf96d2ae051c9694c
+size 321
