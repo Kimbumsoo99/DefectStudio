7f2badb2ad94980f8d37a34755b8fd777c61db00
박수진 <tnwlssla20@naver.com>
2024-09-03 11:08:10 +0900

    [#32] feature: BE rembg router 추가
    #S11P21S001-185
    
    
    Former-commit-id: e88c3e905680e437ff90c378d5595c73b8a7e922

--- backend/api/routes/generation/main.py
+++ backend/api/routes/generation/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9ffe9da5cc13285962eb5fc7da52bc47b9d37208c22908c316c38dc9c678ce00
-size 177
+oid sha256:dd5bc67e965b99c6a80e1ae5b11e034be4cccfeb9c0a011a872bbc275f6a521c
+size 220
