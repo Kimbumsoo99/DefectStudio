fe91d73746d69a2ba45060da4732af0fae51e717
박수진 <tnwlssla20@naver.com>
2024-09-04 15:15:52 +0900

    [#43] feature: 부서별 회원 조회 API 구현
    Former-commit-id: 9aff814f5026936c4f05008a4764daf33e5f193d

--- backend/api/routes/departments.py
+++ backend/api/routes/departments.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:286fb5798023c73fbd91576fed7cb49e210b4a0cabfe181818615e6b9f8058e9
-size 365
+oid sha256:2b586379041252a40a3a6182ea6ac6894057b85e479b347b81e99f28f7dc3fc7
+size 917
