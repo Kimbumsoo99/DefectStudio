cd7a730bc041990344587819f48604e37b95364c
박수진 <tnwlssla20@naver.com>
2024-09-04 16:14:16 +0900

    [#44] chore: crud 모델별 코드 분리
    Former-commit-id: 8bbad29ce798328222a049bcc01f1686f7afc411

---
+++ backend/crud/departments.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:dda5aa5d56f34b56ef03bddbb78380fd58a5756cd707e5acdd9cd74e9920a7c5
+size 340
---
+++ backend/crud/tokens.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:80d2e75684325e4bff7cff6d2075cc79c79351b6e46061e9cd01160876d7ef46
+size 4031
