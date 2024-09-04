a0e074d0bc165f6836ddb7188f0585befe7381ee
박수진 <tnwlssla20@naver.com>
2024-09-04 15:15:41 +0900

    [#43] feature: MemberReadByDepartment DTO 구현
    Former-commit-id: 46b5007e6403ca74899f7133bd674fcfe195c983

--- backend/schema.py
+++ backend/schema.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:befbd2da2162483c05e3936ad45ecc23dd02ae588d0a67c5d14ba57079edadf9
-size 3180
+oid sha256:6eaa8afc1578fd3ae361d928a0c3ee018386ea005d18ee8c8184e11500c9957d
+size 3580
