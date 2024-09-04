5f725562a8e6d2a30c4089949b8daaa7fedd27ce
SnowyPacks <originpc@naver.com>
2024-09-04 22:56:18 +0900

    [#42] feature: Token Page 라우터 및 Navbar설정
    Former-commit-id: 28deaf3669e634811ac65f218e56abd294aa314d

--- electron_frontend/src/components/common/Navbar.tsx
+++ electron_frontend/src/components/common/Navbar.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f742a84bf7b9a2ae49dcad97113c74e85cc8a0670884d92a9b9428ea46d91fca
-size 4600
+oid sha256:90bf3ee56a2edb9e3cc80d2c1f7afb30c3571d426386adda63483896c5e290fa
+size 4604
