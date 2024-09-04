3e3f117ef7bbd3e1626cc8ef816848edd243fdec
garamgim <garamgim@gmail.com>
2024-09-04 17:54:10 +0900

    [#41] feature: Inpainting AI API formdata로 변경 및 오류 수정  #S11P21S001-167 #in-progress
    Former-commit-id: 5f6d3b5cd2967b6848e1000ead12170aaf91a15e

--- backend/api/routes/generation/inpainting.py
+++ backend/api/routes/generation/inpainting.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8ad8c625a04fcbbc63da1cbf441b0233580198efad164887635890363aa5c71e
-size 2939
+oid sha256:ed02d374d88e51829b6f650fdbda473a5be0328ebf7a9e51b64fcd87a9f18178
+size 2958
