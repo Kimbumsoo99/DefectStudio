0f7f771c54aa17626a7b3152f1f22ef22627f615
박수진 <tnwlssla20@naver.com>
2024-09-05 15:11:31 +0900

    [#53] feature: TokenLog quantity, department_id 필드 추가
    Former-commit-id: 27d0068a34df4dcb9967043844d080269b522118

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:db81a7079fe807361426a62ad80b7961f142294bbcf318d5b6ca4f95aa972860
-size 2891
+oid sha256:50b1ecee234235ab298c48f92d1e6b003082daa77b91697537d17bd562be8870
+size 3157
