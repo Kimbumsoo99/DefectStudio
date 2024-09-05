da8501ce789e73e25f4867a29decbca1f52eb005
garamgim <garamgim@gmail.com>
2024-09-05 10:47:31 +0900

    [#46] chore: Login API Response body 삭제
    Former-commit-id: 3d69eb372a95be75f1c9afe9154260b19fe3280c

--- backend/api/routes/auth.py
+++ backend/api/routes/auth.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e4e223d5f392fa045e408abcdc4f3396dee50415620f96ca15ba0771d1f23e45
-size 3610
+oid sha256:fa9f96b97d4b6e0fa4bf4f15a2e27f8f962f994022cfb195673bab3c26bd0b4b
+size 3450
