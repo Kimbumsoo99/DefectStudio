ed617cee28089f92bfae46d1ec79d30507738bc2
박수진 <tnwlssla20@naver.com>
2024-09-05 10:35:16 +0900

    [#45] refactor: 토큰 발급 API 수정
    Former-commit-id: 285eb994089432e4fba5dffdfcb2454303c7de23

--- backend/api/routes/admin.py
+++ backend/api/routes/admin.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7837df99b8e9063e5b17c46e91483ea96c854cf7a8d02968bd482ce811ae6b8b
-size 4493
+oid sha256:725d1ffa63674abfdf851839fcf74dc0e672593610680960653c0598d305a120
+size 4868
