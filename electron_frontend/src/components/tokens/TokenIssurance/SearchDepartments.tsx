15f71f1c4b9a701c54402723af75b408a2d85daf
SnowySparks <originpc@naver.com>
2024-09-05 11:23:55 +0900

    [#42] feature: 폰트수정 및 토큰 발행 칸 구현
    Former-commit-id: 8b93731edefb4ad1efd1c574b020aca793681fc9

--- electron_frontend/src/components/tokens/TokenIssurance/SearchDepartments.tsx
+++ electron_frontend/src/components/tokens/TokenIssurance/SearchDepartments.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b99e15a706cdeb3538b30efc2d569512728e0a67fcf8c325c949f32a2c379858
-size 1120
+oid sha256:ea77da40dd79b6d3994423597886cd08b160c2d18909fdac3f4fd4e8eb409907
+size 1141
--- electron_frontend/src/components/tokens/TokenIssurance/TokenIssurance.tsx
+++ electron_frontend/src/components/tokens/TokenIssurance/TokenIssurance.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:49468a86abfd8a8aec18eb9a995c2703ccddeacbd9fb8aa8ca9a871d375a1d91
-size 247
+oid sha256:cb4ff2d80987bfaa82d7f34bfb6db35d411722cf76881cbea56f58311f70f7a1
+size 322
