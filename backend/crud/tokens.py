56749991998bbf3c50171072c3b6e2ecaec1fb85
박수진 <tnwlssla20@naver.com>
2024-09-05 16:30:22 +0900

    [#53] refactor: TokenLogCreate 파일 경로 변경에 따른 반영
    Former-commit-id: 0213768a72bd5e6c9abd13c4a868db39c771b3f7

--- backend/crud/tokens.py
+++ backend/crud/tokens.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:793e32b558c4adaab608a7ae3c21ea984ab2701a338d385746d1c9a1336b8ae1
-size 4102
+oid sha256:d4565e5fe068b354b8aa1031e87476a196b04a24ede53a8ba8368582163c2a02
+size 4125
--- backend/schema/tokens.py
+++ backend/schema/tokens.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8aa9e2e55f2df184b3ec159dddf09d9757472e59f5a1d7d395afc66f7bcf4e8f
-size 1991
+oid sha256:ed5e7b08112523c5a827d716a586f0815e41c6f13940d2ca65427bc0c59f2fd2
+size 1801
