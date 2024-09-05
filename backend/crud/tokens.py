78c589559913e1693784b5539e2b4621c862f7b5
박수진 <tnwlssla20@naver.com>
2024-09-05 10:57:56 +0900

    [#47] refactor: 관리자 토큰 조회 API 수정
    Former-commit-id: 35ccd7c7cd4b03c02d4c27c4c6f684938804397e

--- backend/crud/tokens.py
+++ backend/crud/tokens.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:84b915625293915f5599cfc21e290803218af23c9af4ed09a821225d82c7ddf0
-size 4038
+oid sha256:cfcba18582961bd7b5cab8a4feb39df67b0da94b72c5956743c2a247ce29592c
+size 4018
