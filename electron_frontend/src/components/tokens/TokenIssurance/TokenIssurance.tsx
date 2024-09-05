5ccd0d53206c6f1be1a6c86ee8ddd4ef46d5db68
SnowySparks <originpc@naver.com>
2024-09-05 10:54:54 +0900

    [#42] feature: 파일정리 및 부서 검색 기능 일부 추가
    Former-commit-id: b28287ad7459e726dbc06d571dd8d1cd597a0628

---
+++ electron_frontend/src/components/tokens/TokenIssurance/TokenIssurance.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:49468a86abfd8a8aec18eb9a995c2703ccddeacbd9fb8aa8ca9a871d375a1d91
+size 247
---
+++ electron_frontend/src/util/getAllDepartment.ts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0dfc9a3a14ef2b5f86f2107c271a5cce7c0ddeb438244ead645521ecca7043b8
+size 392
