b29477f8da00303879e0974f36b69240590d0ffd
surina125 <jhyunsoo1125@gmail.com>
2024-09-05 10:58:31 +0900

    [#30] preset: create preset ui
    - 사이트바에 preset 버튼 추가
    - eslint, prettier 설치함
    - 다크모드, 브라이트모드 모달창 디자인 완료
    - 반응형 완료(사이즈 줄였을 때 custom-scollbar적용되게 antd커스텀 디자인)
    
    
    Former-commit-id: 656e816152640b7d17d7e0c9d8add2ab721e4781

--- electron_frontend/src/pages/Generation.tsx
+++ electron_frontend/src/pages/Generation.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4e606e302c95a406a8c91f30739f9a52f87dc5c22af52ece2db8c9d189e42637
-size 6175
+oid sha256:566717e4686ee1d13056e2a21db1fbc2821ab1989f4806fde7177e003cf61aac
+size 6007
