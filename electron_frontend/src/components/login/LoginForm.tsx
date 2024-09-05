603587146a107f731c3456c6b385ef9dc5e5da15
SnowyPacks <originpc@naver.com>
2024-09-05 22:04:54 +0900

    [#36] test: Header접근 테스트 코드
    Former-commit-id: 895577b6895746f2e533a967a921289ee5ff2ab2

--- electron_frontend/src/components/login/LoginForm.tsx
+++ electron_frontend/src/components/login/LoginForm.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:27bbd4f44a857d2e6ab6f01f4f8b09885022748c2b12e827d2e727e0daa14c6b
-size 2277
+oid sha256:fe0741f41c136d0b74a367b139d774c8b1f61cc286289f059fcfc55d0e120f4b
+size 2291
