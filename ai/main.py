8e1a8973bc53c3fc32df933092ae98cecf09ec24
garamgim <garamgim@gmail.com>
2024-09-06 23:43:15 +0900

    [#54] Cookie 설정 시 samesite None으로 변경
    Former-commit-id: 8213421d5fb30eba54d953b9852187f35639b622

--- ai/main.py
+++ ai/main.py
@@ -1,13 +1,3 @@
-import uvicorn
-from fastapi import FastAPI
-
-from api.main import api_router
-
-app = FastAPI(
-    prefix="/api"
-)
-app.include_router(api_router)
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="localhost", port=8001)
-
+version https://git-lfs.github.com/spec/v1
+oid sha256:24918a755f1075631d4665eaf6c2f778f6c2a3c6951c6ad1d4f7a26d38b79b78
+size 222
