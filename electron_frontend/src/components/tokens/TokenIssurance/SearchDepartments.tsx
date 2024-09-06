f6e068a890c69b298e764a26c268982e9052e792
SnowyPacks <originpc@naver.com>
2024-09-07 02:56:27 +0900

    [#42] feature: 검색기능수정
    Former-commit-id: 77f8dc1cd7dd1d53926b172cbbac1fb93ec6ae4d

--- electron_frontend/src/components/tokens/TokenIssurance/SearchDepartments.tsx
+++ electron_frontend/src/components/tokens/TokenIssurance/SearchDepartments.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a302085c8f9c40e4059af1e6ad31eac367179d4fb51f4c874f3ab95221ff6869
-size 2517
+oid sha256:28364d9e8337445fcc2c576012249352255ed24c9b7cdf8e61cdf11e4139db4e
+size 1986
