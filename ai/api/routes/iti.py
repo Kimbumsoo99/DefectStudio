06ec479146ce4f82aba9f9ccaad4ba063cd67712
garamgim <garamgim@gmail.com>
2024-09-06 23:39:37 +0900

    [#50] refactor: AI Generation API 이미지 변환 코드 리팩터링
    Former-commit-id: ba021afc0ad3a6ee6e3d2f58426ead23858c13ac

--- ai/api/routes/iti.py
+++ ai/api/routes/iti.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:824879aec0d87c0fea0cbaddeca1d4acbb5e3a9fbe8a549986fdd918d6b77287
-size 2063
+oid sha256:ec12b24302fccb247c43a61a38ae6376c01ff0991ddda1bf5b5aa8690e38d952
+size 1763
