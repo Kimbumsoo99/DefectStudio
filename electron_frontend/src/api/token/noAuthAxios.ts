ad9159c46aaa982de873f4c799327ae4d300f3e7
surina125 <jhyunsoo1125@gmail.com>
2024-09-07 06:29:44 +0900

    [#30] login: api 통신
    - 로그인, 회원가입 시에는 메뉴탭에서 로고만 보이게 처리
    - refresh token 만료됐을 때(401) access token 삭제하고 로그인화면 이동
    - 로그인 성공하면 홈화면 이동
    
    
    Former-commit-id: cbe52c3a093ff9402e0666e716f65b0ce3934dd9

---
+++ electron_frontend/src/api/token/noAuthAxios.ts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:26ec6bf4a5fc050972e8d4f286c0756cd75871fea148588bc3d2cca6601cde13
+size 352
---
+++ electron_frontend/src/api/user.ts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c36d41358848205136ab57eaf56e4fc0ff9d5c122da49ecfd6cc8bee889d99c9
+size 996
