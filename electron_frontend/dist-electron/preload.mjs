c73fcdbe2d33102683f3fb16f371021f363ff3d8
surina125 <jhyunsoo1125@gmail.com>
2024-09-07 11:12:24 +0900

    [#34] canvas: konva로 변경
    - 브러쉬, 폴리곤, 점이동, 객체선택 후 삭제, undo, redo 기능 만듦
    
    #S11P21S001-175
    
    
    Former-commit-id: 0d542c38f09ed126e2c5705614da5ad8e5081196

--- electron_frontend/dist-electron/preload.mjs
+++ electron_frontend/dist-electron/preload.mjs
@@ -1,3 +1,1 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:d164faec016492f8ffc7e20d08214c1c2ca7138c93cfe7c59b3f29f30bf74ec4
-size 670
+"use strict";const o=require("electron");o.contextBridge.exposeInMainWorld("ipcRenderer",{on(...e){const[n,r]=e;return o.ipcRenderer.on(n,(t,...c)=>r(t,...c))},off(...e){const[n,...r]=e;return o.ipcRenderer.off(n,...r)},send(...e){const[n,...r]=e;return o.ipcRenderer.send(n,...r)},invoke(...e){const[n,...r]=e;return o.ipcRenderer.invoke(n,...r)}});
--- electron_frontend/src/App.tsx
+++ electron_frontend/src/App.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d7635259845dd69ca48a22821086cc2259068f44bbd03afa43c2a3a14b450efe
-size 2224
+oid sha256:8cdaa7981890e8ce35a2d47c301c02d7b2b4dfe0f96a6992963e3628226dd36d
+size 2241
