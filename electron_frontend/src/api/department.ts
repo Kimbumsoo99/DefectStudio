5ffeadb8f3e3991ad942a700398e665feea314b8
SnowyPacks <originpc@naver.com>
2024-09-08 01:28:01 +0900

    [#42] feature: 부서 내 인원들 검색바 추가
    Former-commit-id: 1e8cd2a238ba20ec1c2d0ae474c5bc27c2c53008

---
+++ electron_frontend/src/api/department.ts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6ce43bad7c7d975e5046a31593ba8a46175f781256dc6c6ffa6db6c3a53db6b6
+size 944
---
+++ electron_frontend/src/components/tokens/TokenDistribution/SearchDepartmentPeople.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7d12daefae4ce78aca2f429af10033ad3eab15a5778e19fdca8f71585bb3e1ab
+size 1968
