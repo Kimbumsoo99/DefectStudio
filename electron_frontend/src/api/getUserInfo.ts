3999368bbcc06ec137fa7dfd9c0a02c52f133ca7
SnowyPacks <originpc@naver.com>
2024-09-07 19:44:10 +0900

    [#56]fix : 롤백
    Former-commit-id: 83a81c448857ded9f4a1b5f21b272432688db22f

--- electron_frontend/src/api/getUserInfo.ts
+++ electron_frontend/src/api/getUserInfo.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5a0372235e9c901768c2f27242a2591702d125bf56ed597880be00337a7dbd5e
-size 606
+oid sha256:a7f08be7fc7b9bdf609367376c31ca7f0f442ec0ac9add07e1bcedbbce4acc37
+size 617
