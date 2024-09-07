c3c1ac782e824dc9ad3226f033e513579ad5c34d
SnowyPacks <originpc@naver.com>
2024-09-07 20:41:08 +0900

    [#56] feature: 유저정보 가져오는 함수 제작 및 적용
    Former-commit-id: 6dd886bc768f12177dc270d46b8768eaab8d3f2c

--- electron_frontend/src/components/profile/MiniProfile.tsx
+++ electron_frontend/src/components/profile/MiniProfile.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bc4aa1e3b9737cea24379801cc23c20c0ab583a23d5ab6e8226fdb699ddb04a3
-size 1873
+oid sha256:b93a6abe9c44c3549f3c979e744021384b8d9ef3f352f13c5f2cc2b4154810de
+size 2241
