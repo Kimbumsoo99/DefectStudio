8be51be20b4902fc453d96b07971048a92ae17b9
garamgim <garamgim@gmail.com>
2024-09-08 20:39:05 +0900

    [#50] feature: Inpainting API 배치 모드
    Former-commit-id: 17e7bf19b61fd434fcffc45563a4fccdc6f592ce

--- ai/api/routes/inpainting.py
+++ ai/api/routes/inpainting.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3ebab75dec232f4213e50f37569669037d857d989687a01ca33900c7f9acfac2
-size 1922
+oid sha256:891a5c7a4342a0b5e80c576f30f3cb78b503a8f4179f8d3e99c3a86367c5a163
+size 2676
