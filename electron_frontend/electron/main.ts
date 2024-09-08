c487e58493430e37d23b278a06a889e0aae78a14
surina125 <jhyunsoo1125@gmail.com>
2024-09-09 06:48:55 +0900

    [#52] batch mode: input directory 가져오기 수정 중
    Former-commit-id: 3edf4884fddaf678dc3304b16e659e9f2568a591

--- electron_frontend/electron/main.ts
+++ electron_frontend/electron/main.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:03953656a2f8a1655703604bd9957e0a4f2bf4d9056135f8328002b948a49830
-size 2787
+oid sha256:d8d0064a575ff1a95f9d0e42684a50af1deab7a93f15a02a32cf8bf3fd34c019
+size 3229
