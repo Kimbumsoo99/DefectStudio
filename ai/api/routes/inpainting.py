7451f5f54aa405a3a1a591044400d2f5ae495e35
garamgim <garamgim@gmail.com>
2024-09-08 21:44:11 +0900

    [#50] fix: 누락된 파라미터 추가
    Former-commit-id: 28af537b03a9d80b191f3be94cd1a8cb94301749

--- ai/api/routes/inpainting.py
+++ ai/api/routes/inpainting.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:891a5c7a4342a0b5e80c576f30f3cb78b503a8f4179f8d3e99c3a86367c5a163
-size 2676
+oid sha256:defaab4b4a09d1220465439cc327338ed052c188fd5911fd207ad0eb88476797
+size 2715
