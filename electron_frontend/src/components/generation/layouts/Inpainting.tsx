2f843c39d3e3fd6f3383a235abed1449bae57b5c
surina125 <jhyunsoo1125@gmail.com>
2024-09-09 09:10:56 +0900

    fix: inapinting 코드 수정 및 주석 해제
    Former-commit-id: 0d5cfe0dde779e24a6957ab6ff3c33aa40af5164

--- electron_frontend/src/components/generation/layouts/Inpainting.tsx
+++ electron_frontend/src/components/generation/layouts/Inpainting.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4e854782b701817434ff3ed59b6ea12f0336520426647d6290da4b565f40ff36
-size 593
+oid sha256:9a45578310fbb92a389e1e202beb6c9dd21ceeaf373bc75a54b9da1848a08adc
+size 536
