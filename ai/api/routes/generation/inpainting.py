ba35dce38642b0ca959228f8ef4a56e6f383d94c
garamgim <garamgim@gmail.com>
2024-09-09 13:31:40 +0900

    [#50] refactor: batch mode시 이미지마다 seed값 unique하게 변경
    Former-commit-id: 52fe7fa5aa751e99f8b7f151b07bdd536c63df23

--- ai/api/routes/generation/inpainting.py
+++ ai/api/routes/generation/inpainting.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:defaab4b4a09d1220465439cc327338ed052c188fd5911fd207ad0eb88476797
-size 2715
+oid sha256:0cd202a3e3e7f8a72d3c58376c57bd1770f4e3feac75aebc87e043ce5a64c91e
+size 2724
