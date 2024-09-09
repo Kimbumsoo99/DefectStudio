e962a2cd770099cf8020982a0c1a0f581b8e5231
박수진 <tnwlssla20@naver.com>
2024-09-09 14:17:32 +0900

    [#59] fix: IOPaint dependency 관련 다운그레이드
    #S11P21S001-278 #in-progress
    
    
    Former-commit-id: 7d31e540f0d831e530c4dabf82f6ed8b1484192e

--- ai/api/routes/generation/main.py
+++ ai/api/routes/generation/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ba4f728c557666dccbcca0b1deeff3b8a932828c2bb80f58b9b804b42f84eed7
-size 369
+oid sha256:f7fd5e4746d12133544ae98f6e1a6135e202bac23baf46f2acb7f0c1237fd25a
+size 385
