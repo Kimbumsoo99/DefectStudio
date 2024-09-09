f3f2bc72e69e1da28e802e9a3031ec749870ffcd
garamgim <garamgim@gmail.com>
2024-09-09 09:33:18 +0900

    [#50] refactor: AI 서버 API url 구조 변경
    Former-commit-id: 165c56a4485f2b9a345aa2bc36dc11ce37479c29

---
+++ ai/api/routes/generation/main.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ba4f728c557666dccbcca0b1deeff3b8a932828c2bb80f58b9b804b42f84eed7
+size 369
---
+++ ai/api/routes/main.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:18c93b515097c24e1a23041c471230866ec6c83212646de506cae4f2ef8ee37f
+size 164
