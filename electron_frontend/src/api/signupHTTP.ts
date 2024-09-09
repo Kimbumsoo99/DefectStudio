d97af4464315814515a94cb6ea5840be085e5538
SnowySparks <originpc@naver.com>
2024-09-09 16:42:06 +0900

    [#42] fix: 자잘한 버그 수정 및 CR에러억제옵션 추가
    Former-commit-id: 31b77dcaca3509343a975029102a6971e6a1b87a

--- electron_frontend/src/api/signupHTTP.ts
+++ electron_frontend/src/api/signupHTTP.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1aa1a51f63cce493cc5fac7a18abc69efd490f863488eeb85173a160cbd05759
-size 404
+oid sha256:cdd3ffca427d816863df00877643ec9a82974f28ed9e1ad49e3d6d3d67fdf2c0
+size 377
