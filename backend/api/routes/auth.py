8674cbdf70e47a9b240b3305be799995da55e181
garamgim <garamgim@gmail.com>
2024-09-09 18:00:50 +0900

    [#58] fix: refresh token 생성 시 불필요한 파라미터 제거
    Former-commit-id: a0383129ccd78cf6118f2c451bcdb4860cd9021a

--- backend/api/routes/auth.py
+++ backend/api/routes/auth.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9c848d5b43dcaaceef6272464c3b0f382d36fca175ca02a1905650af8dee3c66
-size 3442
+oid sha256:2b5c652d8be1530657ba9319a520dfbc1b8fb3dfecf4e5666c63fc48526d4dbf
+size 3415
