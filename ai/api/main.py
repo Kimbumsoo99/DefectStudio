5c9a768e655767a00a10f0abbc51182c07476e10
JaeHunJang <wuddlaa@gmail.com>
2024-09-11 01:45:58 +0900

    [#60] feature: AI util router 추가
    #S11P21S001-285
    
    
    Former-commit-id: 0a0e4217b2bd9d2ef7ffafcf9845640d304f7ab5

--- ai/api/main.py
+++ ai/api/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:27f8486d8a778c77199f781b613effca619718ba3e90dacb7cd8fe002680d631
-size 372
+oid sha256:150913188dc6c5d62dfcbcdfd18175bdc67234c319f59b492ef0c5f2af0f6368
+size 417
