7b8b03462707ee9425826548ac3d54e80344d0b1
surina125 <jhyunsoo1125@gmail.com>
2024-09-10 18:12:10 +0900

    test2
    Former-commit-id: 208c3b8595b439debf21bc162622a693804a59ac

--- electron_frontend/src/components/generation/sidebar/Img2ImgSidebar.tsx
+++ electron_frontend/src/components/generation/sidebar/Img2ImgSidebar.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3ba6bad3935c4e3297da7ccf61bc73a56b82df7ab54d490d1c885aabf83280ed
-size 4090
+oid sha256:1ba53363b424e00458398741b11f8bb3c0ecd8cc17655597c99fc15296385902
+size 4091
