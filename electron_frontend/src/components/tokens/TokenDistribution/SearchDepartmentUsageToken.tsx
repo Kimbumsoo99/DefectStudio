d2f1fe15d3e74c0c58694f62a7bfad7e3b323e55
SnowySparks <originpc@naver.com>
2024-09-10 15:34:39 +0900

    [#42] feature : TokenUsage 테이블 완성
    Former-commit-id: d258a1b93636a8620b7d7a12f6e0aa72a8a115f5

--- electron_frontend/src/components/tokens/TokenDistribution/SearchDepartmentUsageToken.tsx
+++ electron_frontend/src/components/tokens/TokenDistribution/SearchDepartmentUsageToken.tsx
@@ -1,5 +1,3 @@
-const SearchDepartmentUsageToken = () => {
-  return <div>Hello</div>;
-};
-
-export default SearchDepartmentUsageToken;
+version https://git-lfs.github.com/spec/v1
+oid sha256:f2a3cdca942b6b4c418bf2a9b2e8453ba7a6aa04f3cffb286897be457cf177a4
+size 4031
