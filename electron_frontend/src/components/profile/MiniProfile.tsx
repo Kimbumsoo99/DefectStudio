c8f96dfb083b583a223220823257b849f1c4b09f
SnowySparks <originpc@naver.com>
2024-09-10 16:12:38 +0900

    [#42] fix : build Error Fix
    Former-commit-id: 02785d84d6b241dabfee802b5d4457899237e8c5

--- electron_frontend/src/components/profile/MiniProfile.tsx
+++ electron_frontend/src/components/profile/MiniProfile.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6b95d61335248c27b29d65e44e1d9c6ba38e1df1869b9ea221266a284b538902
-size 2260
+oid sha256:e8076908aade8f8ddcc959b9b529e50b0771261fdc6453a7f10df4611f74bc13
+size 2279
