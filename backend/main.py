ab9a830c45d16c160832f3e0b504ffc42001dfd8
garamgim <garamgim@gmail.com>
2024-09-10 22:43:36 +0900

    [#58] fix: create_all 실행 위치 수정
    Former-commit-id: b240ef8614c465a9f6fb9062cff930fea8f109cf

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0c3fde1da9972f394230c2af002e10a75b8f6371a0245e2aafd61a399395ada3
+oid sha256:8df808182a775409e2fa8ebd25de7e4e49c687b4a95c2587ece59b7547d469af
 size 1643
