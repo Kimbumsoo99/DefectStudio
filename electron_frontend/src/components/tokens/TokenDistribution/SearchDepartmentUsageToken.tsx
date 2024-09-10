aec0d9d61f983082ffc5e81f42f4e6faeec8959a
SnowySparks <originpc@naver.com>
2024-09-10 17:59:33 +0900

    [#42] feature: Type변경 및 최종 토큰 분배량 정보 전달
    Former-commit-id: 4af6c834524fea784947828d7568b803d0b34e32

--- electron_frontend/src/components/tokens/TokenDistribution/SearchDepartmentUsageToken.tsx
+++ electron_frontend/src/components/tokens/TokenDistribution/SearchDepartmentUsageToken.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:effde7d5300ab835024bd5b1af9052d325c8166a46319b65301cc8f5b8ae98d6
-size 4008
+oid sha256:8097a61afdaa4b6f7eaf943745a942caa2281a5277ee6d8ff9608ae5a637d1b4
+size 4043
--- electron_frontend/src/components/tokens/TokenDistribution/TokenDistribution.tsx
+++ electron_frontend/src/components/tokens/TokenDistribution/TokenDistribution.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6b54f9b611d2e1577991b889ef5ee7232b553f1652913bcf9ee3324a212a2ed1
-size 3346
+oid sha256:363d111b2a4b4f7e1c5b7d3720c6feb5446351d3bbb1cda648b5fa3eadf6fe3c
+size 3449
