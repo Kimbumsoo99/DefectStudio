6574be898b3f937b1385cd90a73b417a043b3b39
박수진 <tnwlssla20@naver.com>
2024-09-10 15:38:42 +0900

    [#63] feature: Role guest 추가
    #S11P21S001-282 #in-progress
    
    
    Former-commit-id: 1d02f9bf40c6ee7786ee0a39d27589ecf8c384a6

--- backend/enums.py
+++ backend/enums.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:44bb7c4d7b08237a2b305c76bc20ba6dab22319d0ce4bd16e684b6cef65339cb
-size 539
+oid sha256:c3439426d227d0e5a67fe77b768f9684795aafe93e344af0961f924c56a85c00
+size 559
