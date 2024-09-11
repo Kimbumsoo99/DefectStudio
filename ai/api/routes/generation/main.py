c030c8c51bbb70063275ae90c8340ac88922755a
박수진 <tnwlssla20@naver.com>
2024-09-11 16:41:12 +0900

    [#65] refactor: Generation API cleanup 파일명 수정 반영
    Former-commit-id: 0a8dafe661e080fdc05393236474ae1921a479e4

--- ai/api/routes/generation/main.py
+++ ai/api/routes/generation/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0d070023ff482c8e5618863065b5e8ce7cdcc741ac3dd37e5fa359d1e35aa600
-size 426
+oid sha256:4da2e7914d830f6eded3f689fc21d5b54835ee9ec64089824225008b25c09e0e
+size 410
