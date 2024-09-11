86aadc25f3e3223c61c29909ea7adb411e523875
JaeHunJang <wuddlaa@gmail.com>
2024-09-11 22:50:01 +0900

    [#73] feature: GPU Device Set API 호출 구현
    #S11P21S001-304
    
    
    Former-commit-id: d93708b8c032dad79fdfd38acf73d8524c99eaf8

--- backend/api/routes/device/util.py
+++ backend/api/routes/device/util.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9e2b18e17d94ea59d67257f4182059dd4ede18113660afc6ba66733b2b1c578f
-size 1463
+oid sha256:9491385bf2ce3eefa395fdbaf323be9296821db314124bac8091e8b984d94d45
+size 2231
