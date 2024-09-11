d5d64ab52278465e2a230148ed6fd774d5603f3a
SnowySparks <originpc@naver.com>
2024-09-12 08:56:27 +0900

    [#42] fix : 분배 요청 후 자동 재요청 안되는 문제 수정
    Former-commit-id: befe6628cf2cb319204304f8046ef8b1867c5f44

--- electron_frontend/src/components/tokens/TokenDistribution/TokenDistribution.tsx
+++ electron_frontend/src/components/tokens/TokenDistribution/TokenDistribution.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b9ed535d62aebd7eadcd767d3ca172a95dc1952da67e1c328d87e2b3b5db4fa5
-size 5464
+oid sha256:8f2eac405ac7d354f3393eba62e7461dba668b91d1506e2ceb4e1f7158d42c3d
+size 5466
