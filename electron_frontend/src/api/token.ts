ea8536f25dce6840c262d426dfc0e6ef0ee6b2a6
SnowySparks <originpc@naver.com>
2024-09-11 16:54:30 +0900

    [#42] feature:토큰분배기능완료
    Former-commit-id: b258172657864fd20ed4912e861a2c23ed3ebdad

--- electron_frontend/src/api/token.ts
+++ electron_frontend/src/api/token.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f48cc32840b8967ce6c095d70efd01b5427b21d5d8056fc343fc9756fd1d44cd
-size 1223
+oid sha256:a0bf25fa1a9539ae7d0bcfeb38e173398550de1b2243021a810282fca1496a56
+size 1722
--- electron_frontend/src/components/tokens/TokenDistribution/TokenDistribution.tsx
+++ electron_frontend/src/components/tokens/TokenDistribution/TokenDistribution.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:363d111b2a4b4f7e1c5b7d3720c6feb5446351d3bbb1cda648b5fa3eadf6fe3c
-size 3449
+oid sha256:b9ed535d62aebd7eadcd767d3ca172a95dc1952da67e1c328d87e2b3b5db4fa5
+size 5464
