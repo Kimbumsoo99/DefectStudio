c7a97843f37253e2173c9a1ad89ffdcb585aa825
SnowySparks <originpc@naver.com>
2024-09-12 11:20:10 +0900

    [#42 fix: 로그아웃 상태에서도 나의 정보 요청하는 문제 수정
    Former-commit-id: be5c951ecbe377204cb31bee2668dc50e5e7a276

--- electron_frontend/src/api/user.ts
+++ electron_frontend/src/api/user.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:be034ca0276f2953bf33812e485062061ab412561b49f7f801815bca79e979cc
-size 3044
+oid sha256:f016e0e238b6932ea71878e3e671b1e6a0cc4e884cf078ef5c454fde98c628da
+size 3108
