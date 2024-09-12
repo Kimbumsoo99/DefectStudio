96f337b907bceb1d7ad6ef37a6ec0fb2698c49e7
surina125 <jhyunsoo1125@gmail.com>
2024-09-12 14:54:40 +0900

    [#52] refactor: prompt refactor
    Former-commit-id: 2adf0cd38fc45e50cac37ffdb0fa543044319c73

--- electron_frontend/src/components/generation/layouts/Txt2ImgLayout.tsx
+++ electron_frontend/src/components/generation/layouts/Txt2ImgLayout.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6d9ac4acc0e09176a95e6e8c9d5b1887a3a1000e5765810b7f1a4479a2d9e0b9
-size 888
+oid sha256:dbebb0f8055e9acbf1a6de17bfe490cf72ad4a7e828a662d712bd517cb4d61c7
+size 1619
--- electron_frontend/src/components/generation/sidebar/Img2ImgSidebar.tsx
+++ electron_frontend/src/components/generation/sidebar/Img2ImgSidebar.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c60963e367fbf29846cee4e809be0f1e89acccbbcfd21bffb5c066e4811724a8
-size 4425
+oid sha256:9a662525ce2cd8638ba56335614ca720123a1dfba590621c9d9a8f494a1bb4ca
+size 4568
