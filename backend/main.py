40309931204ae1f59261c5a99863faeba120225a
garamgim <garamgim@gmail.com>
2024-09-12 13:22:50 +0900

    [#70] chore: 환경변수 추가 및 초기화 시 URL 변경      #S11P21S001-274 #done
    Former-commit-id: b583a67de6b3191ff4956392a52aac68653eb1a9

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c9c42c5f4d4ea740f5b3f71d846a67dd1ea605ba9624c50fd993579790afe97c
-size 1806
+oid sha256:35048c76ebe60b3e911e89f1be0bc1de582b3cd063720b7f4bfbdf22ba3962a5
+size 2197
