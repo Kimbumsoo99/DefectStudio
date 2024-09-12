92087deac8d6102a3c9992adb784502cb3c84088
garamgim <garamgim@gmail.com>
2024-09-12 13:49:35 +0900

    [#70] fix: Test 클래스 삭제
    Former-commit-id: 30507bd92a1a7dee2c008486c3c2467b1520b400

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:35048c76ebe60b3e911e89f1be0bc1de582b3cd063720b7f4bfbdf22ba3962a5
-size 2197
+oid sha256:4d6a2b1c0d8d9ddf2f93806f6d3a752bf50ed6923189265186718bd442e9a19d
+size 2191
