923de0c572cab2c63df26bc0d63c9507e3463ab9
garamgim <garamgim@gmail.com>
2024-09-12 15:52:38 +0900

    [#70] chore: 주석 추가 및 환경변수 오타 수정
    Former-commit-id: 8a0ef80fd52dba88bf0ac2354f45abb9f8db1ca8

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4d6a2b1c0d8d9ddf2f93806f6d3a752bf50ed6923189265186718bd442e9a19d
-size 2191
+oid sha256:9555eb0116a26f38d6e7600f30114996e64f52233f31efb674bc46a80fb1356a
+size 2301
