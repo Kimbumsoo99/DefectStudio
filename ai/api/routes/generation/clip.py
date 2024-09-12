8ef1ce549e9b84e34baf7e50ce04787c336772a1
박수진 <tnwlssla20@naver.com>
2024-09-12 14:06:29 +0900

    [#65] refactor: model 파라미터 추가
    #S11P21S001-280
    
    
    Former-commit-id: ee0facbbba091f561cfcd7c0d6aaec440304f7e3

--- ai/api/routes/generation/clip.py
+++ ai/api/routes/generation/clip.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:441087d5c78493e6d74ebde24bd7820598b74f5b869b79443a2385b210206b1a
-size 1684
+oid sha256:dc4da20b41bb3b32aab1c6f7f6f274350e7ec372facdb5d97097e85fd75cee57
+size 1702
