ffbab54d1733713d4978bdec637ca4e62b9ccdfe
박수진 <tnwlssla20@naver.com>
2024-09-12 13:52:03 +0900

    [#65] refactor: Generation API clip 리팩토링
    #S11P21S001-280 #in-progress
    
    
    Former-commit-id: 2cfc11a506a001db3cc7e58cceab7c55b034be77

--- backend/api/routes/generation/clip.py
+++ backend/api/routes/generation/clip.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cf3564d70168d70bda059badb2c0cf581f3cfa83438aeb9ba0bb4a9b1d28cc14
-size 855
+oid sha256:e1bacc6f27d9ed2f02b12e915c006db36d8dee94a98e4969175a962365c311ca
+size 1392
