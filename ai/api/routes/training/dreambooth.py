fc998a18843fa317b321c6a6f86964fe0093e800
Kimbumsoo99 <show7441@naver.com>
2024-09-13 13:38:09 +0900

    [#67] feat: AI Server 파라미터 오류 수정
    #S11P21S001-291 #in-progress
    
    
    Former-commit-id: d4c6ec8261b9c60f5e80f084244d1706b399a088

--- ai/api/routes/training/dreambooth.py
+++ ai/api/routes/training/dreambooth.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cb3fc41e70737d61c52789f1ce4eac9af3eabd5f0c0796a8deae53b4e3f9366c
-size 15183
+oid sha256:1c4c6cbef319eee50def35c4039c8266ec267fc62e43154b483401169bc2f179
+size 15391
--- ai/core/config.py
+++ ai/core/config.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:df84c0b411321b95da8e108ac654f6485b93cd3ec590beaaafe6da562da2a6fe
-size 328
+oid sha256:676c10decce1c39c24ee290e6edcdb1b966f20104df4d6be1bfa338e1c137fd7
+size 353
