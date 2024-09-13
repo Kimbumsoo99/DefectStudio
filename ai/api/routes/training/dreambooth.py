6171b64ac1451fe0c6400c03ffa3909fe716fba6
Kimbumsoo99 <show7441@naver.com>
2024-09-13 09:29:35 +0900

    [#67] fix: AI server training router 추가
    - Form 데이터 String으로 받는 문제 임시 해결 (파라미터 정리 후 추가 처리 예정)
    
    #S11P21S001-296 #in-progress
    
    
    Former-commit-id: 230c9b86368021a65d10cf579bcc4266c67b70fa

--- ai/api/routes/training/dreambooth.py
+++ ai/api/routes/training/dreambooth.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4900ba85379782a16f74149117a108f830aebae246cc17918c14ec50310aa406
-size 14615
+oid sha256:cb3fc41e70737d61c52789f1ce4eac9af3eabd5f0c0796a8deae53b4e3f9366c
+size 15183
