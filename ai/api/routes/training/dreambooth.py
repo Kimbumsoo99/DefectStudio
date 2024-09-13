5e2ae1d3be2992ed8caf66038c351d73e391136c
Kimbumsoo99 <show7441@naver.com>
2024-09-13 15:45:45 +0900

    [#67] fix: AI Server GPU 지정
    - 기존 default로 가던 설정 문제 해결
    
    #S11P21S001-291 #in-progress
    
    
    Former-commit-id: 8418eee6f448198fdae18cf6fb7bf17d2116bd72

--- ai/api/routes/training/dreambooth.py
+++ ai/api/routes/training/dreambooth.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1c4c6cbef319eee50def35c4039c8266ec267fc62e43154b483401169bc2f179
-size 15391
+oid sha256:8939f094283204f5477619dd1bb38ac611db621b4416b9ae670c77e9f05a041a
+size 15454
