5138241b518d5c6e5bdf578320366817d182ae16
Kimbumsoo99 <show7441@naver.com>
2024-09-14 01:31:47 +0900

    [#77] feature: Model 조회 API 개발
    #S11P21S001-322
    
    
    Former-commit-id: aaed1b7c98393e5631074693c9a8ea96ae312e0b

--- backend/api/routes/model/model.py
+++ backend/api/routes/model/model.py
@@ -1,5 +1,3 @@
-from fastapi import APIRouter
-
-router = APIRouter(
-    prefix="/model",
-)
+version https://git-lfs.github.com/spec/v1
+oid sha256:7690f48d1cf2f6a43bf1aab07428167fcfbe47ed4fd0c32b77c64a480811e9a3
+size 970
