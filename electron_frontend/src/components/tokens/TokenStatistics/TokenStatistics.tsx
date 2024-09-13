74f73f555bf01cbdbc7f12d6181933377d01f48a
SnowySparks <originpc@naver.com>
2024-09-13 15:10:28 +0900

    [#42] feature : API 코드 작성 및 Statistics 구축
    Former-commit-id: bf8bdded6d80f514b2ffb9a3847c4c1f76014154

--- electron_frontend/src/components/tokens/TokenStatistics/TokenStatistics.tsx
+++ electron_frontend/src/components/tokens/TokenStatistics/TokenStatistics.tsx
@@ -1,11 +1,3 @@
-import TokenStatisticsHeader from './TokenStatisticsHeader';
-
-const TokenStatistics = () => {
-  return (
-    <section className="flex flex-col justify-center align-middle">
-      <TokenStatisticsHeader />
-    </section>
-  );
-};
-
-export default TokenStatistics;
+version https://git-lfs.github.com/spec/v1
+oid sha256:ef78fbfd040e698eab09dcbb929d89a6f209b6f19d5c2585bada93e3e708fe87
+size 749
--- electron_frontend/src/components/tokens/TokenStatistics/TokenStatisticsHeader.tsx
+++ electron_frontend/src/components/tokens/TokenStatistics/TokenStatisticsHeader.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b73f93b3ff71d361264a22365a7916b01ae0c6de4dc525c6624e3125285897b2
-size 517
+oid sha256:7dda69ec6740804fc203ace5fa0ad1d326bc546394de0d3dce674023ede357f2
+size 814
