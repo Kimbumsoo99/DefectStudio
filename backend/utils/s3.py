b7faea277fb49c425819c4806db775bc7409d7a1
garamgim <garamgim@gmail.com>
2024-09-16 18:55:55 +0900

    [#76] refactor: S3 업로드 메소드 파라미터 타입 변경  #S11P21S001-275 #in-progress
    Former-commit-id: 8566066a441088842c9252553ceaf8435d10b240

--- backend/utils/s3.py
+++ backend/utils/s3.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b858f74d8deaa605c615710fd84dc67394ad33fa89858d0493607281dd97810a
-size 2193
+oid sha256:5f760b3a18277d6becba93ae93da7c4893820d5f1663bb0934b541a2cb323eaf
+size 2259
