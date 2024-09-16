f399d04be9b89118fe388e34da12024495c22636
garamgim <garamgim@gmail.com>
2024-09-16 20:37:04 +0900

    [#76] feature: Preset 저장 시 로그인한 유저 id 자동 입력 및 Generation API seed 범위 조정   #S11P21S001-275 #in-progress
    Former-commit-id: 2f5243ebe0dd99d15681fe39c445c3e3357e406c

--- ai/api/routes/generation/inpainting.py
+++ ai/api/routes/generation/inpainting.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cd103bcb93db7cedaf8ed6360d9bf96a29231f4684eb7abff0c43cd9e04e3cc3
-size 2849
+oid sha256:fdf3ff06c4ccfe4535b74e16dba1519636e887e0e43c7a02a47a3f135bc2c541
+size 2863
--- ai/api/routes/generation/tti.py
+++ ai/api/routes/generation/tti.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3a78cf3154e251c61a5473bdd7bcb79e5ee170965a36c363597efd3f46cc0e37
-size 2026
+oid sha256:8b3f2accd776007d675a2ce66a59163cab2f329c910d870ecb3bf8f23c5108c5
+size 2040
