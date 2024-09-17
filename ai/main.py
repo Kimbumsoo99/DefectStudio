0cc83ac5d8437c31e6b5a4cf6529eac43a6544fe
JaeHunJang <wuddlaa@gmail.com>
2024-09-17 23:45:02 +0900

    [#78] feature: add ai sentry_sdk init and tags
    #S11P21S001-298
    
    
    Former-commit-id: 4a6c35be6e6d5ad2bdbfdd0ddc25b587f95ad5d7

--- ai/main.py
+++ ai/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:930acac17ebc1858a83caead5aae9e896ac9dfb31b97093a92614a8a5c5d5032
-size 1348
+oid sha256:3b318565bdf2f3fcd254ba5151935d42de51f62711916d53e9bfb89019cfb41d
+size 2165
