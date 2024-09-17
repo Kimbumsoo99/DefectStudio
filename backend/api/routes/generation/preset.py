125bfce0382606ba7ea6191d53b2a15a161e0b39
garamgim <garamgim@gmail.com>
2024-09-17 12:40:12 +0900

    [#75] feature: Preset 상세 조회 API
    Former-commit-id: 4a363e8c4ae491d78fc71d34f643f4fe3b4c1068

--- backend/api/routes/generation/preset.py
+++ backend/api/routes/generation/preset.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:02a548af45ce576e6f2b0fd5c4ac992721bbe123af3809b8307292ea4a7b50a7
-size 2067
+oid sha256:b659ff47bab516a3a98b7f7fd9e3b40c47e102f41aac19b70e585666c700f2ef
+size 2592
