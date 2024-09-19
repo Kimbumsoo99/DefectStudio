77efe042b6d13d89e5f2886393db6e35e46bcc06
SnowySparks <originpc@naver.com>
2024-09-19 16:38:49 +0900

    [#42] feature : 개인 토큰 기록 정보 큰틀 UI구현
    Former-commit-id: d3af072731106651b21015f8b218c2bf6c5c00dd

--- frontend/src/api/statistic_person.ts
+++ frontend/src/api/statistic_person.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:25420733f5b22e6c503dde54d3f02494f073c29c7a5f22e39c2f527f303e7b4c
-size 1411
+oid sha256:c68650045f7cbde393db9e374ecb1009c6fac3f1f8ed0897f4b19711745d08ed
+size 1457
