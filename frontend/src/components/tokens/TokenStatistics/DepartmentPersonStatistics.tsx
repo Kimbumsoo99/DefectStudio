1c60031072d71116128841b9d614273f2e0d0ef7
SnowySparks <originpc@naver.com>
2024-09-19 15:36:40 +0900

    [#42] feature: 개인 통계 인원선택
    Former-commit-id: 3e4f8ed4849e55e69274398cc635fa3105f373c0

--- frontend/src/components/tokens/TokenStatistics/DepartmentPersonStatistics.tsx
+++ frontend/src/components/tokens/TokenStatistics/DepartmentPersonStatistics.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5cf706670d8787304015aa12ff03ae2d9503b00a33dfd8974cd058f0881dc7fb
-size 583
+oid sha256:cde83c6fca3dc32361827922a384a2b55c52e63c85af0bf199f8653575aebf9e
+size 2350
--- frontend/src/components/tokens/TokenStatistics/DepartmentStatistics.tsx
+++ frontend/src/components/tokens/TokenStatistics/DepartmentStatistics.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cae9b237fae03463710d5f8e233fde2f3ea7da3f924ceaa107d714a845bd3838
-size 2004
+oid sha256:b40f93073a6af27577d8fd914e5a59d28b22d22ce18f1dce400f8c71ffa3f1e3
+size 2032
