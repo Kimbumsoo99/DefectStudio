255457b288f9b3e592de0f57b3d21ffc7c6598b7
SnowySparks <originpc@naver.com>
2024-09-19 09:43:39 +0900

    [#42] feature: 개인에 대한 토큰 요청 코드 추가
    Former-commit-id: 74b9fd79cffb82aec219214ac3db738dbc546d17

--- frontend/src/api/statistic_department.ts
+++ frontend/src/api/statistic_department.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:41ff39501579cfdd1b97279acb421d861dbbfc79ee4ca734e975965fcd41b707
-size 2190
+oid sha256:e6af2b29aba4073c52fbd6edcb1cd2247f1f30783083146d87b3cfb2ec201d76
+size 2415
--- frontend/src/components/tokens/TokenStatistics/DepartmentPersonStatistics.tsx
+++ frontend/src/components/tokens/TokenStatistics/DepartmentPersonStatistics.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f1f0faaa38ab3c5651d008eed6d75ff0b297370b33d545f47598f68103b03ad0
-size 246
+oid sha256:05e50516622cfe2197a467f8d42c04e09e6cfc46d89a17b1fee9422f6b744f02
+size 610
