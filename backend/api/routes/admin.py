eda9efe9b321ad73c08282291511164869fe5700
박수진 <tnwlssla20@naver.com>
2024-09-19 10:59:31 +0900

    [#83] fix: DTO 삭제에 따른 import 삭제
    Former-commit-id: 21108b9c65f929f99c20539414bc486a90602927

--- backend/api/routes/admin.py
+++ backend/api/routes/admin.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:80e228e36c23840484f6446e5f1d6ca68e801d40c80719d5c40cc51ec133c115
-size 7698
+oid sha256:d2514bc2f0a1ba57d4d0ea0c44ee2c496f7a3f2414066f5c5a3cd885c0d32fbd
+size 7682
--- backend/crud/token_logs.py
+++ backend/crud/token_logs.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c09eee552f8fa36b8d6af9c37b06fb2b9637381eed928cef1079fbde2340ac55
-size 1459
+oid sha256:8238cb2f42b270a8d8fd0b5a2e1cbac068edbdb54607b84ae89ca7c3eadd8b2a
+size 1443
