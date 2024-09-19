134b04b403a9e32a9f01ed68dbef6c6d26b87ee4
박수진 <tnwlssla20@naver.com>
2024-09-19 12:12:14 +0900

    [#86] fix: 토큰 조회 로그 파라미터 수정
    Former-commit-id: d72f55313c42a1d66460e54786fb76aab1a3ed13

--- backend/crud/token_logs.py
+++ backend/crud/token_logs.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8238cb2f42b270a8d8fd0b5a2e1cbac068edbdb54607b84ae89ca7c3eadd8b2a
-size 1443
+oid sha256:a70eb92a9fe5a3b06ada7c26ae9f806e55025dba85c11ba23eca9b8e02c106c5
+size 1511
