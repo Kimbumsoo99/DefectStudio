c29bcefc93d176173a3985be412de7433b88b203
SnowySparks <originpc@naver.com>
2024-09-19 14:12:49 +0900

    [#42] feature: 부서단위 기록 요청 큰틀 구현
    Former-commit-id: bcb4b03427ec0fbae8e9a3ef5695ba625b70818a

--- frontend/src/api/statistic_department.ts
+++ frontend/src/api/statistic_department.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e6af2b29aba4073c52fbd6edcb1cd2247f1f30783083146d87b3cfb2ec201d76
-size 2415
+oid sha256:dba90c2bcc959dcb13c80106ae4a472f91262e2c2531cf2c8702f118f41f7a66
+size 2706
