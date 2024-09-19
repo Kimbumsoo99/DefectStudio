bfe6244ccdf7aca06b0b907cdb90c1e131f5542c
박수진 <tnwlssla20@naver.com>
2024-09-19 11:04:35 +0900

    [#85] fix: 토큰 사용 파라미터 변경
    Former-commit-id: 38e80930c713f8afb70bf8ccdb2b00e5bed3d154

--- backend/api/routes/members.py
+++ backend/api/routes/members.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0a0de1f51654cba42c079395a274cae80e46b94b7cdc71d83682904b0dcd2a15
-size 4839
+oid sha256:9cbeb0a150fec521168301916ce6d3e1aa29d3688839fd77b3958330d8585936
+size 4890
