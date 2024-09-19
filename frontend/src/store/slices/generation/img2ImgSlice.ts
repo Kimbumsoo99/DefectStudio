fcd6f96eec9336f7899888eec32cb22e45bf054a
surina125 <jhyunsoo1125@gmail.com>
2024-09-19 16:35:56 +0900

    feature:  preset 컴포넌트 연결, load preset 기능 추가,  preset 삭제 api연동
    #S11P21S001-366
    
    
    Former-commit-id: 07e7a91ecc2a78b2bf2c178dbb979302ac21078e

--- frontend/src/store/slices/generation/img2ImgSlice.ts
+++ frontend/src/store/slices/generation/img2ImgSlice.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:65cb3a8ca56b887fa339c075025fc650a59ad31286cc40eab1e6694f6810ddbb
-size 4307
+oid sha256:ef205f2398b05a7a058e41a798a05d74844ea8b8836f7ae3d9887a5a84a42c6b
+size 4296
--- frontend/src/store/slices/generation/inpaintingSlice.ts
+++ frontend/src/store/slices/generation/inpaintingSlice.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aa4890f7ac4c01e48afe006a35bc30d0a9789be507a7303fadc857bc93356202
-size 4687
+oid sha256:0b56b5522ae7f9e7fffc62f68ce94c6ad4e39cfbae0e3d62c6d6f55a587019c8
+size 4676
