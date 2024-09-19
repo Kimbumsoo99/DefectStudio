2210ebd86b03abaaa42b95e5481a994c096ce6a8
SnowySparks <originpc@naver.com>
2024-09-19 17:24:45 +0900

    [#42] ui : 통계부분 Tabs로 수정
    Former-commit-id: c1f12a15cf0ce9b6b13c2ed7e01315572075f15c

--- frontend/src/components/tokens/TokenStatistics/StatisticSection.tsx
+++ frontend/src/components/tokens/TokenStatistics/StatisticSection.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3c688c5179bb916983334b05c497b9869d97ea1d9029602488fb485c6313655a
-size 1436
+oid sha256:8e69a75da9c691e424f29514e214c9dfc4cdf456b1fa33c6c02aea63954b909b
+size 822
