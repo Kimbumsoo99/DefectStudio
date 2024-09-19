e39c9c670dc5fa8bb37e844ffdaef3d8617e6a96
박수진 <tnwlssla20@naver.com>
2024-09-19 14:15:24 +0900

    [#84] refactor: token_log 필드 추가에 따른 수정
    #S11P21S001-347 #in-progress
    
    
    Former-commit-id: 1063535db8fcd52a9e81a4c54ed4367fa125a35f

--- backend/schema/token_logs.py
+++ backend/schema/token_logs.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0bc1c6bd6fdeee489f74b4a8f4c3640ec15714642738dafdff467c23bbb9da53
-size 1040
+oid sha256:45e2b737d3e10efd6aff45f910a3c54ffc4fd57612dab6713348a21c36c6b6b0
+size 1240
