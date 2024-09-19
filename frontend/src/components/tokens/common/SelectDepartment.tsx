5780af62c9985888387e5b1d2e58232b0ab66051
SnowySparks <originpc@naver.com>
2024-09-19 17:50:40 +0900

    [#42] fix: 불필요한 변수사용제거
    Former-commit-id: 07087451f3e7f363877385976bcfb1b78dc21132

--- frontend/src/components/tokens/common/SelectDepartment.tsx
+++ frontend/src/components/tokens/common/SelectDepartment.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2e310ea7d1a4eb218a7024c4667cb26e18027cf688a8787c34de9ba63fc96772
-size 2132
+oid sha256:c415a1f09f271fd6202c82f63de3e531414e887f8b9f38b55fd08812a15176a9
+size 2112
