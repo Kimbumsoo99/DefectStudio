9370406783285e66637fe603aaa91125fe27419a
박수진 <tnwlssla20@naver.com>
2024-09-19 10:34:23 +0900

    [#83] fix: 토큰 사용 로그 조회 파라미터 변경
    Former-commit-id: b4c7af3dea150787f92b077313b645938ced5bae

--- backend/api/routes/members.py
+++ backend/api/routes/members.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0a0de1f51654cba42c079395a274cae80e46b94b7cdc71d83682904b0dcd2a15
-size 4839
+oid sha256:0d8ba4b967c5ca8571b1bdeb78cfad3158df214c663185bab41538540da1eade
+size 5064
