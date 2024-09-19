f16f94b4e46b1c6029352175149cb258dae23765
SnowySparks <originpc@naver.com>
2024-09-19 09:43:39 +0900

    [#42] feature: 개인에 대한 토큰 요청 코드 추가
    Former-commit-id: 077a5bb29837aa06ec06cdb2b3815418cf3dd256

---
+++ frontend/src/api/statistic_person.ts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:77a97b1e419654e36017da935cc0bd93a38c9b2f0e5369142728659d1453f772
+size 1080
--- frontend/src/components/tokens/TokenStatistics/DepartmentPersonStatistics.tsx
+++ frontend/src/components/tokens/TokenStatistics/DepartmentPersonStatistics.tsx
@@ -1,9 +1,3 @@
-interface DepartmentPersonStatisticsProps {
-  departmentId: number;
-}
-
-const DepartmentPersonStatistics = ({ departmentId }: DepartmentPersonStatisticsProps) => {
-  return <div>{departmentId}</div>;
-};
-
-export default DepartmentPersonStatistics;
+version https://git-lfs.github.com/spec/v1
+oid sha256:05e50516622cfe2197a467f8d42c04e09e6cfc46d89a17b1fee9422f6b744f02
+size 610
