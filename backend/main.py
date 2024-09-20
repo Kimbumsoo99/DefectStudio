8e56a51b89db97205dd02105e6841580127523e5
garamgim <garamgim@gmail.com>
2024-09-20 15:25:44 +0900

    [#81] chore: celery 분리 및 main.py 정리  #S11P21S001-352 #in-progress
    Former-commit-id: 8b792a2d5d619700408f6c69383c3b511856d6c5

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1ca7cc9b37bb5466af6e005b88bf592c6bf1cbea4cce3696bfaab37adce50b02
-size 3100
+oid sha256:16194efacdbc8415bb03a80b417e96be92befb2527077af91eebc1bc2d759dde
+size 2811
---
+++ backend/utils/celery.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d7824672357a53a721fa3d783d409081fd26a81abbd4a9bde1dea44261224f80
+size 231
