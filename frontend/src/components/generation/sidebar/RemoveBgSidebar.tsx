2f5e1c0a48ce5e6d9f96a82b90bae4af9c7382a9
SnowySparks <originpc@naver.com>
2024-09-20 10:32:18 +0900

    HOTFIX : Merge 작업중 누락 및 버그 코드 픽스
    Former-commit-id: f0497ca2e23188323dc772ab385eba8e06883111

--- frontend/src/components/generation/sidebar/RemoveBgSidebar.tsx
+++ frontend/src/components/generation/sidebar/RemoveBgSidebar.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:84fad63b7a8c73676c06f4fa06d2fcd02f88d511202e691aa97813de37c9b490
-size 2369
+oid sha256:1dd2be494a338800161896925db03c48d97b906021b708dd99e58a1ed4ed7d09
+size 1646
