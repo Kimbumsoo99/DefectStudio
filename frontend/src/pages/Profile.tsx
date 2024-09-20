54ef48ad9bb5d5ea21aadcc59be6910c1542714a
SnowySparks <originpc@naver.com>
2024-09-20 13:57:12 +0900

    [#82] fix : 프로필 Fix
    Former-commit-id: df3382d7ef7fff3e232fa17b021047a947f752c8

--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9066cd7dc5f1f16ffad813b1d2ec785e0b10366c3ef6bc38b0fb32c9832097ff
-size 3868
+oid sha256:13c6068d9df0dee23a8a06d82ae3a89af2599da66f75d76e3d0c9c31167ba782
+size 3793
