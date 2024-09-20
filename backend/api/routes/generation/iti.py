44fea0895ccf93f2907ed6ffc148741c433e275a
Kimbumsoo99 <show7441@naver.com>
2024-09-20 15:28:15 +0900

    [#93] refactor: i2i & t2i 이미지 생성 BASE MODEL 변경
    #S11P21S001-354
    
    
    Former-commit-id: be4c8a20621cc9d645a6eba809e85d65869fbf0d

--- backend/api/routes/generation/iti.py
+++ backend/api/routes/generation/iti.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:74b7bdf5b04c79823e8230d0f0d621eba5b4ca9ef61cd3e30a21f073084a56a7
-size 4319
+oid sha256:db17ca720779ebdb64944376de5b50cd2e8209fa49301cbfbb1d1d06d4e40fdd
+size 4428
