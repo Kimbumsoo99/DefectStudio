ffd734e9798110e883b774c4290dbc3616851dc4
박수진 <tnwlssla20@naver.com>
2024-09-20 16:27:34 +0900

    [#96] feature: 임시 회원 거절 API 구현
    Former-commit-id: c72247abb7651a4c395e0c58677aeedf02f23512

--- backend/api/routes/admin.py
+++ backend/api/routes/admin.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2089e405d79fc76b0fad81f44f12b3312a66042eec364d7e91c9a08c24d15a05
-size 6844
+oid sha256:d38333a7a57afa4df0e95ea3afc5d4dc20b8fb7f796e66f03ae8af0dff31e977
+size 7602
