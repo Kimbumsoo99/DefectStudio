1bec4420e95b4347123d82d1e54a7f9723112112
SnowySparks <originpc@naver.com>
2024-09-20 15:02:09 +0900

    [#82] feature: 승인&거절 버튼 추가
    Former-commit-id: f85b62f4e3a525e52d524f63d778164dfb9b53c7

--- frontend/src/components/guest/GuestUserList.tsx
+++ frontend/src/components/guest/GuestUserList.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8539dd86a2ea255ddf073ed9a7a087d3db59b12ef3ec737103a5a541651b91c3
-size 5366
+oid sha256:fe81e1dff476b99ae03f8d6a08c2f7cda9b12f8887dc96badf5a0a54139a3f18
+size 5687
