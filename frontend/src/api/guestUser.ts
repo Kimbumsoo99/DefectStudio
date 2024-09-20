0ade7a5984723ebb4d47c82a9fcab643fb2d70c9
SnowySparks <originpc@naver.com>
2024-09-20 16:43:42 +0900

    [#82] test: API 요청 테스트_성공
    Former-commit-id: 644151814e49d44ced620baebd941bcd78eb7deb

--- frontend/src/api/guestUser.ts
+++ frontend/src/api/guestUser.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:db38ba8687d2afd59b7fa733302b5b8114a142d1b9a72b6802423cd8830c4450
-size 1641
+oid sha256:b98ddcc1f32e6080391f677bb240383ac23ac7b0cdb2f651035e315bba480e52
+size 1677
