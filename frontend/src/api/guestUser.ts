e93610dd343757fc83fc4341acbb59dac29d5e71
SnowySparks <originpc@naver.com>
2024-09-20 16:32:39 +0900

    [#82] feature: Guest User API 통신 코드 작성 및 적용
    Former-commit-id: 20c00ed557408f7c6340ac350047a2855954c1a4

--- frontend/src/api/guestUser.ts
+++ frontend/src/api/guestUser.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7f9267c062a7aec27724c8fe1455b5912cb7f8f70cbb2647a2c817d299821a77
-size 1530
+oid sha256:db38ba8687d2afd59b7fa733302b5b8114a142d1b9a72b6802423cd8830c4450
+size 1641
--- frontend/src/components/guest/GuestUserList.tsx
+++ frontend/src/components/guest/GuestUserList.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fe81e1dff476b99ae03f8d6a08c2f7cda9b12f8887dc96badf5a0a54139a3f18
-size 5687
+oid sha256:bef347bb9ee87bba4905ae098fd9109fc8890f2424ad51e43a435b15bc9eb7fc
+size 5562
