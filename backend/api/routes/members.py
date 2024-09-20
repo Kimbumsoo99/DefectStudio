22c0a3fc79ed82fcd1c7a1a0282580f67a601fef
박수진 <tnwlssla20@naver.com>
2024-09-20 15:54:44 +0900

    [#95] refactor: 기존 토큰 로그 조회 API 삭제 및 추가 구현
    Former-commit-id: 4de0c8a713c91d2b2d5c88476cf22e05151330f7

--- backend/api/routes/members.py
+++ backend/api/routes/members.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1502fd885782f3c97d3db6d3b7ac88bb89e40766038d4aa2980a4795c78399dc
-size 7543
+oid sha256:413152602ddead1ed20f8384a6a60426ed772949df2229c1fd7b8b0164c45c9c
+size 8066
