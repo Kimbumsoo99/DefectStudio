3c05ea30d440c88fc68343bb7324bbfe6767f441
garamgim <garamgim@gmail.com>
2024-09-21 21:50:10 +0900

    [#81] feature: 작업 큐 및 Generation API 호출 Task 정의  #S11P21S001-352 #in-progress
    Former-commit-id: ffe2c67215786167b9849a4a0bdc0ff53621e5f2

--- backend/utils/celery.py
+++ backend/utils/celery.py
@@ -1,9 +1,3 @@
-from celery import Celery
-
-from core.config import settings
-
-celery = Celery(
-    __name__,
-    broker=f"redis://{settings.REDIS_HOST}:{settings.REDIS_PORT}/0",
-    backend=f"redis://{settings.REDIS_HOST}:{settings.REDIS_PORT}/0"
-)
+version https://git-lfs.github.com/spec/v1
+oid sha256:c45a045b82469cb769f0ef2fdd18834ea9c8457eeebafa5f2afd897d54ee4dcc
+size 1739
