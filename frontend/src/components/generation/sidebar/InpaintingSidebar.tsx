70ff1022630b095f8be4725a1c3d9fab2e92b938
surina125 <jhyunsoo1125@gmail.com>
2024-09-22 11:26:29 +0900

    [#98] fix: api통신 필드명 수정사항 반영, inpainting default모델 변경 반영
    Former-commit-id: be1568f12023f08a72fad7d8ed510d9579995427

--- frontend/src/components/generation/sidebar/InpaintingSidebar.tsx
+++ frontend/src/components/generation/sidebar/InpaintingSidebar.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6fabcd3c8ca8a50eca14b695f176f26dcaa7bb11c788b46145de9ac769a0150a
-size 9048
+oid sha256:e1701c4458f72b68bf39ec14f773823f0431c9640fba9fda86c61a52eb0b4075
+size 9058
