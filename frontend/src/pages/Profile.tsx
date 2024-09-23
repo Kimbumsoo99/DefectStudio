f4c3a4193bfb3e704e40db8ea6798ab1f17180b8
SnowySparks <originpc@naver.com>
2024-09-23 16:27:50 +0900

    [#100] feature: 회원탈퇴 기능 추가 및 API 연동
    Former-commit-id: 818ba023f052669f7503927f41da4ff27b6cabd1

--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f910bc8ad3e2bc7d2439c469fac1e0acb2f7abc2f5abd5586ee52d59363d3a82
-size 3551
+oid sha256:7f851f7d61067e06dca5b8d0d23e5234f6196aa5b75d68b0b7cc7ae1e40c6ead
+size 4060
