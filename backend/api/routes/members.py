47fe957f6c0f7e84be4e13149be0efa00b60b114
박수진 <tnwlssla20@naver.com>
2024-09-23 15:38:08 +0900

    [#97] feature: 사용자별 활동 내역 API 구현
    #S11P21S001-426 (#in-progress)
    
    
    Former-commit-id: 5b705636e141f06219d461dc613b113c7e25d87d

--- backend/api/routes/members.py
+++ backend/api/routes/members.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1628a355b978c6754a3d7590a4d366b5d7cf33c8aa8e7cae4968336eecc2b2ce
-size 8015
+oid sha256:028b427a21f954dc7ed448414c627b2b6faed2a48768a58e45d67a8c5297ede6
+size 10044
