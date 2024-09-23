31c2a83226b40159634400c0fb475de5419d5e96
박수진 <tnwlssla20@naver.com>
2024-09-23 16:01:06 +0900

    [#97] refactor: 사용자별 활동 내역 API 수정
    Former-commit-id: b9995d7b35f0234804ad0a337cfef597d03bb2ee

--- backend/api/routes/members.py
+++ backend/api/routes/members.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9ae1f6ca619e67038060b4587029fbce25f46361da2cacaedc1c124460790691
-size 10044
+oid sha256:fc972533c336e6888dbf5a11351ef05c39914ffe8695f42ab6d0a47ba59d08d6
+size 9958
