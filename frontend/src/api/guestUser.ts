4aa5cb4e50b9fc3e30a1c29b5f631e8ced5ee018
SnowySparks <originpc@naver.com>
2024-09-23 10:02:31 +0900

    [#82] feature :임시 회원 관리자 거절 함수 구현
    Former-commit-id: e453e94493c277e6622db6225042e1c793533cca

--- frontend/src/api/guestUser.ts
+++ frontend/src/api/guestUser.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b98ddcc1f32e6080391f677bb240383ac23ac7b0cdb2f651035e315bba480e52
-size 1677
+oid sha256:156734ac596b7e493ed1f0e35959e9d3cbfc11b9399b297604c3a22e6f56b784
+size 2417
