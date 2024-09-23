56d47661872c9c1cd8dd3c069a07edd48a665ad7
SnowySparks <originpc@naver.com>
2024-09-23 16:08:56 +0900

    [#100] fix: 사용한 queryClient를 이용하지 않는 문제점 고침
    Former-commit-id: 2908ecacecdb988697f1fe9ebc0872ea1308cc84

--- frontend/src/api/user.ts
+++ frontend/src/api/user.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f1c02a529544fea60259fe33960dcd621efd38ed7b5e48ab7f5b09bf1e16d175
-size 3729
+oid sha256:5bf4e6211f20bc509d2801eaf6c901558f1f0c1d421af71ddff66ac30f940f39
+size 3671
