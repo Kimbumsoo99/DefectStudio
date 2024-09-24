d18ec2b8363d92f846d07860d4c39da2d15a1478
SnowySparks <originpc@naver.com>
2024-09-24 15:06:41 +0900

    [#105] fix: 파일 전면 수정
    Former-commit-id: 647459daa47a7d039e4b82bf6540f3fc7092a1dd

--- frontend/src/api/statistic_department.ts
+++ frontend/src/api/statistic_department.ts
@@ -1,3 +1,1 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:dba90c2bcc959dcb13c80106ae4a472f91262e2c2531cf2c8702f118f41f7a66
-size 2706
+// ReFactor
--- frontend/src/api/statistic_person.ts
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:c68650045f7cbde393db9e374ecb1009c6fac3f1f8ed0897f4b19711745d08ed
-size 1457
