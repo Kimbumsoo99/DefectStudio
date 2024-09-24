c24c12e3fd3c605208e2ceaeb77940cbb14199fc
surina125 <jhyunsoo1125@gmail.com>
2024-09-24 15:15:36 +0900

    [#107] feature: training api 통신
    #S11P21S001-463
    
    
    Former-commit-id: 6d5e995f9236df5a4dfb0103d4c6b8f91a9b0f50

--- frontend/src/api/signupHTTP.ts
+++ frontend/src/api/signupHTTP.ts
@@ -1,11 +1,3 @@
-import { signUpFormType } from '../types/user';
-import noAuthAxios from './token/axiosInstance';
-
-export const signupHTTP = async (data: signUpFormType) => {
-  try {
-    const response = noAuthAxios.post('/members/signup', data);
-    return response;
-  } catch (error) {
-    throw new Error('Signup failed');
-  }
-};
+version https://git-lfs.github.com/spec/v1
+oid sha256:f341640c973085c6c2111f20a4fb214fdfd726e7240a05d896f350596c913be7
+size 316
