88af2176e4e107464f526c7e64c5fcf23cdd01b1
surina125 <jhyunsoo1125@gmail.com>
2024-09-24 22:21:55 +0900

    [#109] hompage ui 기능연결 ui디자인 완료
    - 반응형, 모달창 내 프롬프트 복사 가능, 브라이트/다크모드 적용
    
    
    Former-commit-id: 4da8492a6bce3a12eddff9d35f367495554723c1

--- frontend/src/components/home/ImgModal.tsx
+++ frontend/src/components/home/ImgModal.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:966ddc9b3baa2690e39edb561323d7b9ff61ac0b0254eb19ede8557c4fdce2e2
-size 3725
+oid sha256:be51126d43de3b0ab28308f8afe180e4d2005c1aff803de2c74b34c6f2fbacef
+size 5287
