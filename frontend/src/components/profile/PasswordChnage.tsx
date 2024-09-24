4794008f0ef570d1b74d9c25d65cb8d2115989bc
SnowySparks <originpc@naver.com>
2024-09-24 09:42:09 +0900

    [#100] feature: 비밀번호 변경 기능 추가 및 API 통신
    Former-commit-id: 2c503ba3b4cd877d1b0772c21730c8a2dd3969ee

---
+++ frontend/src/components/profile/PasswordChnage.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:98a0e4718148c2fb0fc625b5985020b730e8947f7d94cecd6dc541458660db3d
+size 3175
--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7f851f7d61067e06dca5b8d0d23e5234f6196aa5b75d68b0b7cc7ae1e40c6ead
-size 4060
+oid sha256:b7333c482802b6756087c6c8d11162c559e0db08a8a66a5ae64522272af79185
+size 4588
