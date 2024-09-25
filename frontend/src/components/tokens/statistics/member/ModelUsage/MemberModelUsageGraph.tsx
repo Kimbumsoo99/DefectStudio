e4d4cbffdfaae3f16dbd1b1005e7c6ccce4abc98
SnowySparks <originpc@naver.com>
2024-09-25 17:11:23 +0900

    [#105] feature: UI 통일
    Former-commit-id: 1d29f3b87e346719dac406632a26ecea631b492e

--- frontend/src/components/tokens/statistics/member/ModelUsage/MemberModelUsageGraph.tsx
+++ frontend/src/components/tokens/statistics/member/ModelUsage/MemberModelUsageGraph.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ba17e26f8cf93ba6eb2e87df8b1277370c96a22442493b0c7e96de53fcc040b9
-size 1359
+oid sha256:3f72aeed60d1ea2f2844a49c45e113c6c55d5dd8271a892bd5f12fb9fffe5bab
+size 1456
