a1aeb54513d5d9b70db4e2c4bb74d68762f1e95d
SnowySparks <originpc@naver.com>
2024-09-25 12:28:00 +0900

    [#105] feature: Member ToolUsage 구현 완료 _ 막대기그래프
    Former-commit-id: dbc171990e0c03537cfc2141a4878a6286e9422b

--- frontend/src/components/tokens/statistics/member/ToolUsage/MemberToolUsageGraph.tsx
+++ frontend/src/components/tokens/statistics/member/ToolUsage/MemberToolUsageGraph.tsx
@@ -1,11 +1,3 @@
-import { ToolFrequency } from '@/types/statistics';
-
-interface MemberToolUsageGraphProps {
-  data: ToolFrequency[];
-}
-
-const MemberToolUsageGraph = ({ data }: MemberToolUsageGraphProps) => {
-  return <div>{data.length}</div>;
-};
-
-export default MemberToolUsageGraph;
+version https://git-lfs.github.com/spec/v1
+oid sha256:3d3f379a957f52b200dff50b7037a03d0a1ae6637d9c10bea28d630cf47935bb
+size 1503
