ab0b321549a4eec332681fa5ffb027bc0a152f85
SnowyPacks <originpc@naver.com>
2024-09-25 22:42:04 +0900

    [#105] feature: Department Member Image 통계 구현
    Former-commit-id: 155ff1c796a38e888f4b9ecb4f749a89f8deec1d

--- frontend/src/components/tokens/TokenStatistics/DepartmentStatistics.tsx
+++ frontend/src/components/tokens/TokenStatistics/DepartmentStatistics.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7acb6d3f29f35c6ed7a38a2423609f367ebbf9267ea3911a3eeb0aed367f9c8c
-size 225
+oid sha256:8345dc2d12060f90528e8830a9fca0f17d116e2917566d28ac8b1c7b637d6216
+size 613
