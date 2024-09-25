1e52c9fa1e64fe1c513dd1e4f0aac90869c3532a
SnowySparks <originpc@naver.com>
2024-09-25 15:42:17 +0900

    [#105] chore: Member 통계정보는 자기자신만 보이도록 설정
    Former-commit-id: 11d07c3b41dad54acf1d9155892137e29222baba

--- frontend/src/components/tokens/TokenStatistics/PersonStatisticSection.tsx
+++ frontend/src/components/tokens/TokenStatistics/PersonStatisticSection.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cd6941f9453bc9e2e9279f1bc20d44692515854a5ff17605dc72df2612263002
-size 1138
+oid sha256:3168b756107b829ad2f823ec7efa5e9a744ffbd39de8b138e49ba27b78d9ae1e
+size 1224
--- frontend/src/components/tokens/TokenStatistics/StatisticSection.tsx
+++ frontend/src/components/tokens/TokenStatistics/StatisticSection.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b738e75a0a556d6d96051662771308ca4a7ff3cdeef3f67d1d2b96c4b44cbbba
-size 823
+oid sha256:6bd4702c6d74bb819bdc88d8787878e245e544cbed8e50c73adf6d738268916c
+size 789
