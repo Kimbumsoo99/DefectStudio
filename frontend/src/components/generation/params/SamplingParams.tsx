86bbe174010d100a18e4a24da032a120f825aeea
surina125 <jhyunsoo1125@gmail.com>
2024-09-25 21:01:07 +0900

    [#98] fix: generation 하나의 api가 세개로 분리되어서 맞춰서 수정중
    Former-commit-id: 99c9d0e97cdfeb00df175ac7f7d51890bbcb0400

--- frontend/src/components/generation/params/SamplingParams.tsx
+++ frontend/src/components/generation/params/SamplingParams.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:75a5f3e9705045cddf22afc63f2950c6ddb713c84afddfc1566f61584d653063
-size 1995
+oid sha256:b0ec7236dd1ee83f5b09f31a694d9dedd585778af5a8cd62161f015e66f7511c
+size 2822
--- frontend/src/types/generation.ts
+++ frontend/src/types/generation.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:750a39d7d6dadac2c0fc374d230fb2b654216b92d48de67a3013bfb94417f523
-size 3110
+oid sha256:4d165e4e0e74a4c09c316052e7b0e6b700063dc3ae535cb62f7a37ebb89b5597
+size 3154
