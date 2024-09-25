ef6ef0baab197a798317082240172462182f64b7
SnowySparks <originpc@naver.com>
2024-09-25 10:51:08 +0900

    [#105] fix : UseType 누락 픽스
    Former-commit-id: 3f32cf2bb4736153573d932da1076ac16b57942a

--- frontend/src/types/statistics.ts
+++ frontend/src/types/statistics.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f447052f0ad07175918054291ae1b0017451b8fcd544e7fb540f4a25115e65c2
-size 1348
+oid sha256:c39eaf1d1026932cfdb3e76ba700018150f12c68c7e8c7d898f0b0a2c7ba4236
+size 1363
