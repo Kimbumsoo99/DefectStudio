88fa23add0dcf1e18d195b2c184bcb0ce9b97a4a
SnowyPacks <originpc@naver.com>
2024-09-25 23:19:00 +0900

    [#105] feature: 부서별 Tool 사용통계 컴포넌트 구축
    Former-commit-id: 6cd0f554ecd94e4f6c16ebd28438f228d2d2b85c

--- frontend/src/components/tokens/TokenStatistics/DepartmentStatistics.tsx
+++ frontend/src/components/tokens/TokenStatistics/DepartmentStatistics.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8345dc2d12060f90528e8830a9fca0f17d116e2917566d28ac8b1c7b637d6216
-size 613
+oid sha256:3c55c7249e71728d3daab3ca2d52882b7625fb0ada8dd515a091768f6a65cafa
+size 814
