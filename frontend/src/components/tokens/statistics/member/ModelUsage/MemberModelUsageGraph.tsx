884f65ad76d3f427efc3c8cc5858e5a64c508b6a
SnowySparks <originpc@naver.com>
2024-09-25 13:46:21 +0900

    [#105] feature: Model 통계 구현
    Former-commit-id: 90e22ccbfd3bdd026b6658183131462a12fcb306

--- frontend/src/components/tokens/statistics/member/ModelUsage/MemberModelUsageGraph.tsx
+++ frontend/src/components/tokens/statistics/member/ModelUsage/MemberModelUsageGraph.tsx
@@ -1,11 +1,3 @@
-import { ModelFrequency } from '@/types/statistics'; // Response Type
-
-interface MemberModelUsageGraphProps {
-  data: ModelFrequency[];
-}
-
-const MemberModelUsageGraph = ({ data }: MemberModelUsageGraphProps) => {
-  return <div>{data.length}</div>;
-};
-
-export default MemberModelUsageGraph;
+version https://git-lfs.github.com/spec/v1
+oid sha256:ba17e26f8cf93ba6eb2e87df8b1277370c96a22442493b0c7e96de53fcc040b9
+size 1359
