0cc0a857f07b7f8c557eadc75e85f5035e0a1e02
SnowySparks <originpc@naver.com>
2024-09-25 09:38:49 +0900

    [#105] feature: Member Image 생성 통계표 컴포넌트 구축
    Former-commit-id: bfa52e19cdced9b39ad00830523613e38cd0c9ee

---
+++ frontend/src/components/tokens/statistics/member/ImageUsage/MemberImageUsage.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:98823f753dd7cbe5f2b35b5e07edbfc4380b7866b5a21d3c75841b581c8df9ad
+size 1970
--- frontend/src/components/tokens/statistics/member/MemberImageUsage.tsx
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:6b04270ee67f9486644a3b82d96f75602ce4ac02bd09cec40e893c9dc2c51b1c
-size 1134
