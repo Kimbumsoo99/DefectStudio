9b0d5f4212b11b0aed21ef375a5e9e0ed153be47
박수진 <tnwlssla20@naver.com>
2024-09-25 13:11:20 +0900

    [#110] feature: 토큰 차감 구현
    Former-commit-id: 41b4c256f2b7d7d994387034f4b06779b8e92327

--- backend/api/routes/generation/inpainting.py
+++ backend/api/routes/generation/inpainting.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2e0bc8aaab8014ca1ae4c96fbef94e551a8ccc5fc8544c8d7d9966a12f742b24
-size 5252
+oid sha256:fc81415a7521dbc46a4f1d7fc8c735253e21454b15f195c57cfaad01fa50fb3a
+size 5911
