cc314a145937dd602cc78fff3210059e99dfc53b
SnowySparks <originpc@naver.com>
2024-09-25 10:31:59 +0900

    [#105] feature: Member Tool Usage 구현 및 Header Fix
    Former-commit-id: e298a4764b2df64fb41b703f3e76657169ce9d9f

--- frontend/src/components/tokens/TokenStatistics/PersonStatisticSection.tsx
+++ frontend/src/components/tokens/TokenStatistics/PersonStatisticSection.tsx
@@ -1,15 +1,3 @@
-import MemberImageUsage from '../statistics/member/ImageUsage/MemberImageUsage';
-
-interface PersonStatisticSectionProps {
-  member_id: number;
-}
-
-const PersonStatisticSection = ({ member_id }: PersonStatisticSectionProps) => {
-  return (
-    <div>
-      <MemberImageUsage member_id={member_id} />
-    </div>
-  );
-};
-
-export default PersonStatisticSection;
+version https://git-lfs.github.com/spec/v1
+oid sha256:e7299708292d589ed30f977583236037f5fb32c91b4f9251aef12acf0849a65a
+size 756
