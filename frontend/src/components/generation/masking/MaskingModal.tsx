a609ac875462f8599950709599cf432750970032
surina125 <jhyunsoo1125@gmail.com>
2024-09-26 06:56:54 +0900

    [#98] fix: generation 상태관리, canvas 수정, 리렌더링 리팩토링
    Former-commit-id: dd85fb7780f6261a23174798770bd23c756a6624

--- frontend/src/components/generation/masking/MaskingModal.tsx
+++ frontend/src/components/generation/masking/MaskingModal.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a03c8bf66a45ee87bb30fc09effd799fea812632eaa308e5a0bc0e720d5b18a7
-size 23471
+oid sha256:9d0e317417d3814107a030f6980ca38067a90333a3debf8c6c5e2b1a0faa58d2
+size 24776
