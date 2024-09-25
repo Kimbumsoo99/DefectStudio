57d512958f0211554779c0f067fd1b4944cbac17
garamgim <garamgim@gmail.com>
2024-09-25 23:04:40 +0900

    [#81] fix: celery task status 조건문 추가  #S11P21S001-418 #in-progress
    Former-commit-id: a2b4fdf5bb52b081904580ecabc9959ae30f0cbf

--- ai/api/routes/generation/main.py
+++ ai/api/routes/generation/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f2797407ea6f2ebb2440f148aed077885dfd8a59d7e6328b2f69edd1e776de15
-size 2209
+oid sha256:93adc83525386f7359d8273dc311775437991f356b3a0ffcce35482d2ff0a221
+size 2313
