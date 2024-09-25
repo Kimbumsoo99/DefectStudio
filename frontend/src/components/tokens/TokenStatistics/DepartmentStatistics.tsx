3d2ea927969f1af0ba424b77316bd7f861c104b5
SnowyPacks <originpc@naver.com>
2024-09-25 23:41:51 +0900

    [#105] feature: 부서 내 분배량 그래프 컴포넌트 구축
    Former-commit-id: 5cbe2f6bc0a5db2507b5df07f398f5a69c2c34e0

--- frontend/src/components/tokens/TokenStatistics/DepartmentStatistics.tsx
+++ frontend/src/components/tokens/TokenStatistics/DepartmentStatistics.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3c55c7249e71728d3daab3ca2d52882b7625fb0ada8dd515a091768f6a65cafa
-size 814
+oid sha256:4dc98d561223bd804f860f303513eb3194e10c43cdcfe74364bdc8dfd1da5c24
+size 1088
---
+++ frontend/src/components/tokens/statistics/department/DistributeState/DistributeState.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3970287314f98dbd65e187497e07483b2a016440d20a32c997dde3ad982a74e1
+size 1361
