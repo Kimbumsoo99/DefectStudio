69c93d2ad683e87baa5100c4438deb3e0e4bcaa9
SnowySparks <originpc@naver.com>
2024-09-26 16:38:26 +0900

    [#105] chore: staleTime, gcTime 통일화 및 값 조정
    Former-commit-id: 371ea1767732a5d5ca71305148baa94a011e0fb5

--- frontend/src/components/tokens/statistics/department/DistributeState/DistributeState.tsx
+++ frontend/src/components/tokens/statistics/department/DistributeState/DistributeState.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3970287314f98dbd65e187497e07483b2a016440d20a32c997dde3ad982a74e1
-size 1361
+oid sha256:4dda069cd27d10cd3a8032b0c92c562afbcd015ab4350c5090e7513fbd613091
+size 1448
