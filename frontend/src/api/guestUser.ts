7cd61a801f00100a4d4948d737ac30e626b15f41
SnowySparks <originpc@naver.com>
2024-09-26 21:23:50 +0900

    [#117] chore: 임시 회원에 대한 api fix
    Former-commit-id: 876c15a3d93085c5bdeea482bcedfef12d71f94a

--- frontend/src/api/guestUser.ts
+++ frontend/src/api/guestUser.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d273598ad763da484e26b3ce461935c402e511d258cd33a538e849a393dd070c
-size 2499
+oid sha256:f0336f6a75e71ca757d10bf29b1b13e0c9df262b42934d46be7bef38b00c730f
+size 2485
