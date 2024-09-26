03552ce3fbc61aee3d8d8fd566ea033b6f71dce1
박수진 <tnwlssla20@naver.com>
2024-09-26 16:37:24 +0900

    [#114] refactor: 회원 권한 변경, 삭제, 임시 회원 조회 API 수정
    Former-commit-id: 7700c5bf0418c1e243123522e69effb00664a329

--- backend/api/routes/admin.py
+++ backend/api/routes/admin.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:64cab99cf76a810aea788b22d4d448ca4a5c715cfcaf7fa41293162f03d5526b
-size 7650
+oid sha256:9ac101ccd4751a9471af24e9e151416d642653f6ebef2e67eff420b84cf0f9fe
+size 5683
