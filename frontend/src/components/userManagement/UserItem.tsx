6dbe852e72be0c61505a649ca6a90f8141a4c0c9
SnowySparks <originpc@naver.com>
2024-09-27 09:29:03 +0900

    [#117] chore: 버튼 단어 수정 및 Default Value 추가
    Former-commit-id: 82fd4817df21a866dc5b65158c7e097631c584e9

--- frontend/src/components/userManagement/UserItem.tsx
+++ frontend/src/components/userManagement/UserItem.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:af3f2dfbae224353f789be9e96ee464c25184191f578a0e05d2156677cb151e3
-size 2973
+oid sha256:7af839fab888304a8f4a72d387869486ccf6ec8784d383ef93b581c494195a33
+size 3013
