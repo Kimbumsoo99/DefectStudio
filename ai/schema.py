88f4d8a19a4cfe31e9a699e4cab5032529a2dd43
garamgim <garamgim@gmail.com>
2024-09-27 17:52:39 +0900

    [#76] fix: Task Arguments에 이미지 존재 시 발생하는 오류 제거
     #S11P21S001-421 #in-progress
    
    
    Former-commit-id: cb96479095f9e2ba9bba0a7495a2eb95ff3b8531

--- ai/schema.py
+++ ai/schema.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:904152b1a75dae2f3826a0d83aa8a7121deef099db605bbaafcfb89a59325187
-size 310
+oid sha256:8e89207eea709325b867490c0d24b4cca97dd81c046a6698af92d0ab12e695a5
+size 702
