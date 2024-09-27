21878d644c1811276b4bfe1831e149f94511fc23
surina125 <jhyunsoo1125@gmail.com>
2024-09-27 17:05:30 +0900

    [#98] fix: 사이트바 컴포넌트 연결 수정
    Former-commit-id: 152dad2ee597dcbd85d1b52750167d54c5cacfc4

--- frontend/src/components/generation/layouts/InpaintingLayout.tsx
+++ frontend/src/components/generation/layouts/InpaintingLayout.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a9f0831b9659b942601a8280efe675b57123d021b649d426dd1ab86989810f83
-size 9201
+oid sha256:67b2f36d79fdd05040a8cea1be4afef967f111d0fa85630745f995d4b7a820d8
+size 9403
