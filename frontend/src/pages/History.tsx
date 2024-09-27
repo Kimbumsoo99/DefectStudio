a34ad2199e62330a56dffbb5d6258370d002e46d
surina125 <jhyunsoo1125@gmail.com>
2024-09-27 09:46:09 +0900

    [#98] fix: txt2img 수정된 api연동
    Former-commit-id: 052097548239ca9bd9e8a24fee77df2f1dc42af8

--- frontend/src/pages/History.tsx
+++ frontend/src/pages/History.tsx
@@ -1,9 +1,3 @@
-import React from 'react'
-
-const History = () => {
-  return (
-    <div>History</div>
-  )
-}
-
-export default History
+version https://git-lfs.github.com/spec/v1
+oid sha256:90f468f947be8a31886132be68451c1a934e32418ce1899f499a2b87b9c73f62
+size 7643
--- frontend/src/store/slices/generation/txt2ImgSlice.ts
+++ frontend/src/store/slices/generation/txt2ImgSlice.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d581deca219c8d43337185efa12d0c7175a13ff523468e40649d982009feb323
-size 4512
+oid sha256:5b028b2c7be79b008ee771d212b0bc506a37d2730b988bdb05cde5f37a489c15
+size 4675
