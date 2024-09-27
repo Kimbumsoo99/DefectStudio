28775cf889cc41e78d8384e001f49ef1c6debbf1
SnowySparks <originpc@naver.com>
2024-09-27 14:29:45 +0900

    [#120] fix: 회원가입 페이지 전반적 수정 - css 에러 픽스
    Former-commit-id: 7487e33b3582bd635b401a083945d87d4b2c7b5a

--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3a0a5c7a86cdd8abe84768c7b2294317485bf3ac0a083b067d0cf2c2dc751fe0
-size 5821
+oid sha256:753ee8ff34680a7e9b0e7535f02c116d5151ba5cbceb6431ace69a3d31a6a08f
+size 9482
