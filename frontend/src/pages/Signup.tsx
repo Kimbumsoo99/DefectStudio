c78cf970616644f4a3ecfc80b1cb8225b7517336
SnowySparks <originpc@naver.com>
2024-09-27 15:38:28 +0900

    [#120] feature: Email 인증 페이지 구현 및 타이머 구현
    Former-commit-id: f0b7f5ff09cf05f4d4c133e0d96791e9e8570d56

--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:753ee8ff34680a7e9b0e7535f02c116d5151ba5cbceb6431ace69a3d31a6a08f
-size 9482
+oid sha256:98eca985a974b343819b7ab4bcbaae0d3f39cffac4286ea9a5893d37529e8ed2
+size 1494
