5cb7002ee15ac9ec8fab44861b8cd52125140810
박수진 <tnwlssla20@naver.com>
2024-09-27 16:46:44 +0900

    [#113] refactor: s3 delete 비동기 변경 및 리팩토링
    Former-commit-id: 54a681a7df02bb0629ad98fb18fe3092b0e1871e

--- backend/utils/s3.py
+++ backend/utils/s3.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fb5a0d79826d6704bf47ac214dcd2d02f86b6974ce36e0decf7f1725a9d2eca8
-size 3986
+oid sha256:f4aead89bb3af47c10725db31a4d8a5480d365f25341f35e07b6414da6f7a404
+size 2409
