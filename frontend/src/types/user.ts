ce5f16431631efceb485a782252a57a2b88db271
SnowySparks <originpc@naver.com>
2024-09-27 10:03:22 +0900

    [#117] feature: 이메일 인증 api 구축
    #S11P21S001-439 #in-progress
    
    
    Former-commit-id: 4c1e9047edc1db13faf9d5c4a1aa0ad13a30c6cb

--- frontend/src/types/user.ts
+++ frontend/src/types/user.ts
@@ -1,3 +1,13 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:54523b4d624850d4276752b51bf3717265bebda70d6a850439dd2319c760d006
-size 226
+export interface signUpFormType {
+  login_id: string;
+  password: string;
+  name: string;
+  nickname: string;
+  email: string;
+  department_id: number;
+}
+
+export interface loginData {
+  username: string;
+  password: string;
+}
