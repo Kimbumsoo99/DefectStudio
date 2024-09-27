73a24939f2d4455957fe3e91ee5b9bf6ad08d4a2
박수진 <tnwlssla20@naver.com>
2024-09-27 16:52:56 +0900

    [#113] refactor: 안쓰는 boto3 import 삭제
    Former-commit-id: 4c2ab4f1b7ff1c908982bfaf9c0f9d90cf4f06df

--- backend/utils/s3.py
+++ backend/utils/s3.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f4aead89bb3af47c10725db31a4d8a5480d365f25341f35e07b6414da6f7a404
-size 2409
+oid sha256:cc7560eb8e6fe6c1da155a3e7b1714f732a55a23e6be670ee32c824aef308171
+size 2376
