99c70acb7c31fdf564d268edee7f8a5bb1e4d8e1
surina125 <jhyunsoo1125@gmail.com>
2024-09-27 17:05:30 +0900

    [#98] fix: 사이트바 컴포넌트 연결 수정
    Former-commit-id: ec788451fab368265fcbd760288738d7d384add2

--- frontend/src/components/generation/masking/MaskingModal.tsx
+++ frontend/src/components/generation/masking/MaskingModal.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:421426eeb4356bf9e76b419aecaab5694b337d2db9dfee97fdbe5328f5a268d4
-size 26076
+oid sha256:21253c9fe13774493940c67cae8d95285e0f6bf4e746542d9b0081c8e066eaae
+size 26075
