c2554a73acb7f5b866cfaf2e7e6e572011b5b770
garamgim <garamgim@gmail.com>
2024-09-27 17:52:39 +0900

    [#76] fix: Task Arguments에 이미지 존재 시 발생하는 오류 제거
     #S11P21S001-421 #in-progress
    
    
    Former-commit-id: 7bd3321cf35c67d59cf1d9ca260950e400c12c5e

--- ai/schema.py
+++ ai/schema.py
@@ -1,12 +1,3 @@
-from typing import Optional, List, Union
-
-from pydantic import BaseModel
-
-
-class CeleryTaskResponse(BaseModel):
-    task_name: str
-    task_status: str
-    task_arguments: dict
-    result_data_type: Optional[str] = None
-    result_data: Optional[Union[List[str], str]] = None
-    message: Optional[str] = None
+version https://git-lfs.github.com/spec/v1
+oid sha256:8e89207eea709325b867490c0d24b4cca97dd81c046a6698af92d0ab12e695a5
+size 702
