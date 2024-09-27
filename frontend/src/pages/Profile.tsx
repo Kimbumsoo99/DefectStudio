b8c0f239b43d827a602adaa0042755f9cffc94e5
SnowySparks <originpc@naver.com>
2024-09-27 09:30:04 +0900

    [#117] chore: 버튼 용어 수정
    Former-commit-id: 19fd0c7bf64136e5bdb69b2815a53065f9f2adc2

--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b7333c482802b6756087c6c8d11162c559e0db08a8a66a5ae64522272af79185
-size 4588
+oid sha256:fcf9b435c5199be3d67db0cb501418eeacc4ae16f3edb5d76fcece26794d7982
+size 4589
