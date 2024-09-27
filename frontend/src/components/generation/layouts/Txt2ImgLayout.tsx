db3151787943525399cfd3d618be47f85032a86c
surina125 <jhyunsoo1125@gmail.com>
2024-09-27 16:21:16 +0900

    [#98] fix: generated된 이미지 도구모음 컴포넌트 분리
    Former-commit-id: 724362fcd44129a5beaa9ccdbbd4f987dab49bed

--- frontend/src/components/generation/layouts/Txt2ImgLayout.tsx
+++ frontend/src/components/generation/layouts/Txt2ImgLayout.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9d0d14a03d4a7ac20406f7d0b2d3ab913d1a6b69b430344d3819cf0a3e07ed73
-size 13396
+oid sha256:341fad35b957fab18a617393718915b84738f70b56416873d3deb89dfadea9a8
+size 6216
