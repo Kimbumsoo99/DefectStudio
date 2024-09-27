aca20e503e4b7fdacdfbe02259f3889731d7991c
SnowySparks <originpc@naver.com>
2024-09-27 09:33:52 +0900

    [#117] chore: 성공시 뜨는 메세지 수정
    Former-commit-id: 75f6df7d48716a6c5c429ad29826daba2aeae87b

--- frontend/src/components/userManagement/UserItem.tsx
+++ frontend/src/components/userManagement/UserItem.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5fed698bf285d5466f1f39aa1af42071707fbc2d5b19fd7716caa9593e291504
-size 3098
+oid sha256:63cef8f43def430ab7028415772a0cf6f1f20cabba4f68e879edde21debb30ee
+size 3099
