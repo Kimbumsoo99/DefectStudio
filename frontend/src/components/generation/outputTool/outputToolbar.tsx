eaee5155973658a4491ec0e5556eef3b7efb3742
surina125 <jhyunsoo1125@gmail.com>
2024-09-27 16:21:16 +0900

    [#98] fix: generated된 이미지 도구모음 컴포넌트 분리
    Former-commit-id: 46e9e24b8b1ad97176a64046475177709956fd95

--- frontend/src/components/generation/outputTool/outputToolbar.tsx
+++ frontend/src/components/generation/outputTool/outputToolbar.tsx
@@ -1,5 +1,3 @@
-const outputToolbar = () => {
-  return <div>outputToolbar</div>;
-};
-
-export default outputToolbar;
+version https://git-lfs.github.com/spec/v1
+oid sha256:6fa275e0e8e9404d8c8769c3264df387433f7ac7d41cb1ac9b9e463c078a9296
+size 8189
