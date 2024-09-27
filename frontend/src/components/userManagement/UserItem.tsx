509a749e5b6027be20e222a09e6e7b1dfaad76b6
SnowySparks <originpc@naver.com>
2024-09-27 09:32:14 +0900

    [#117] fix :Query 무효화 코드 수정
    Former-commit-id: 1176831ed73ed5e2d5df557ef3b203cc17e34766

--- frontend/src/components/userManagement/UserItem.tsx
+++ frontend/src/components/userManagement/UserItem.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7af839fab888304a8f4a72d387869486ccf6ec8784d383ef93b581c494195a33
-size 3013
+oid sha256:5fed698bf285d5466f1f39aa1af42071707fbc2d5b19fd7716caa9593e291504
+size 3098
