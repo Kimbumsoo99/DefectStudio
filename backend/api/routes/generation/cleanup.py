50a0f7766d97b3191d4552b3b56b01ce638ef6bc
박수진 <tnwlssla20@naver.com>
2024-09-27 10:13:55 +0900

    [#119] refactor: 토큰 차감 수 변경
    Former-commit-id: ef55d32f99266e0081ed8dda19ab08bfd7c2aed2

--- backend/api/routes/generation/cleanup.py
+++ backend/api/routes/generation/cleanup.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2e1622fde9bbec38768c341938afe166c1253b0cf38391922423375cea761369
-size 2341
+oid sha256:7526be7752a2ac8ac6a35af5af10fd98352b4b6eedc10000a482bd3b2ffa8654
+size 2554
