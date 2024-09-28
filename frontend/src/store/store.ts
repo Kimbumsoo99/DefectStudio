ec4025f215441d7aa84b6ec3891177dd98ca9685
surina125 <jhyunsoo1125@gmail.com>
2024-09-28 14:24:56 +0900

    [#98] feature: generation output상태관리 추가
    Former-commit-id: 1bb8a34db1b1d81c1d898cb452e238592e31a2a2

--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:71c6744aa0cf1ed943ca2f5fb061f0b1042c80a8f0a4e705e854d3ced194321c
-size 1000
+oid sha256:fabadf17b3fb271de487a839b944b6fd15cba0e3288b2415e51ed5863d0394ca
+size 1601
