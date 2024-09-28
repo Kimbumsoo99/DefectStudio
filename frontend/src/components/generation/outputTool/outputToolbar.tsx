74b9329cdcbbbaa1509ac8a89b953215c5217ef1
surina125 <jhyunsoo1125@gmail.com>
2024-09-28 16:26:32 +0900

    [#98] fix: 수정된 사항에 맞춰서 txt2img, img2img, inpainting, cleanup, removeBg 상태관리 수정
    - 각 탭에서 gpu선택 추가 <- 선택 안할 경우 settings의 default gpu number
    - 과거 이미지도 누적해서 보여주도록, 최대 100장
    - 생성된 이미지와 연관된 상태 관리 추가
    
    
    Former-commit-id: 668e5f54002125a3e8011e23a164b2b7dac25288

--- frontend/src/components/generation/outputTool/outputToolbar.tsx
+++ frontend/src/components/generation/outputTool/outputToolbar.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4865b6fcd945c0cceb66f7b694222e0216309e159a63e94880f5a3f9d8044483
-size 8238
+oid sha256:d57bda14a3460edc8705f7cde470057aad59f420daa58fc39c9096abea335366
+size 7376
