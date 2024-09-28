0a596401c0b6171089ea456188ee672cc6d65988
surina125 <jhyunsoo1125@gmail.com>
2024-09-28 22:22:13 +0900

    [#98] fix: inpainting api 필드 수정
    Former-commit-id: 7fe30f36bcc93fbe88e4e60d6ecef91820750496

--- frontend/src/components/generation/outputDisplay/InpaintingDisplay.tsx
+++ frontend/src/components/generation/outputDisplay/InpaintingDisplay.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8c6bc23a8760eba432b7bac08b74011e3bf1983a16603a8de95b169cbea4e6c8
-size 3679
+oid sha256:ba4dfa548033bd82aaf4a72dcbe65d77f74a791c591ab52078cb7461cf909cd6
+size 3676
