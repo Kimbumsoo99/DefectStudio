9d15782054c9467715427bf2323e57acecb406cd
surina125 <jhyunsoo1125@gmail.com>
2024-09-28 14:20:26 +0900

    [#98] feature: generation output상태관리 추가
    Former-commit-id: d483a6b00de50314c3973005a25d70326d35c331

--- frontend/src/components/generation/outputDisplay/Txt2ImgDisplay.tsx
+++ frontend/src/components/generation/outputDisplay/Txt2ImgDisplay.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:568f20f28bd3fcf1dae7e05c0f3df11201eb219ae487ea7c79b162be0797c219
-size 4568
+oid sha256:336814c05bf31b41615e1c528eafab81f409716f90a404e6013af407335b4f8a
+size 4552
