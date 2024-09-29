73cc949b01d53a9053ea85929d7517b8edd163c2
surina125 <jhyunsoo1125@gmail.com>
2024-09-29 18:49:09 +0900

    [#98] fix: 이미지 생성 에러 처리
    Former-commit-id: 70ad034967a4d9a3e7ac9df856fa44b289e22a1b

--- frontend/src/components/generation/sidebar/InpaintingSidebar.tsx
+++ frontend/src/components/generation/sidebar/InpaintingSidebar.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:62a4d1ba1bfad57994cd6cadf2e6819c02fe757ac223a7d88ec10d6e110aa09a
-size 10333
+oid sha256:331243a0e4eec4ef7ea618fb8f06c86c3f1d379e56bf843b7525149768429bb3
+size 10774
--- frontend/src/utils/convertStringToFile.ts
+++ frontend/src/utils/convertStringToFile.ts
@@ -1,14 +1,3 @@
-export const convertStringToFile = (base64Data: string, fileName: string, fileType: string) => {
-  // base64 데이터를 디코딩
-  const byteString = atob(base64Data);
-  let n = byteString.length;
-  const u8arr = new Uint8Array(n);
-
-  // byteString을 Uint8Array에 할당
-  while (n--) {
-    u8arr[n] = byteString.charCodeAt(n);
-  }
-
-  // Uint8Array를 사용하여 File 객체를 생성하고 반환
-  return new File([u8arr], fileName, { type: fileType });
-};
+version https://git-lfs.github.com/spec/v1
+oid sha256:1a31d3f674e4d98c3754ec717a3b9906feea445288f7ed972260f8003739cb61
+size 765
