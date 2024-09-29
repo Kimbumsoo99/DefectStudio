a4dc82bb321d11245a0d8882473e791c3888feb5
surina125 <jhyunsoo1125@gmail.com>
2024-09-29 23:05:11 +0900

    [#122] feature: history 목록 조회 api연동
    Former-commit-id: 592eaf01d2ef72669d43bac8817a155430d7c234

---
+++ frontend/src/api/history.ts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:70d2116ea866ddb4aa82ffaa4fd5888eb6f0c71ebef0430c981c31fd3b9f5c33
+size 444
---
+++ frontend/src/types/history.ts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:20e1fbe84c6a7dc515eac3e525e3de2c0f3ef3c81304596f0ed83722f4763ffd
+size 257
