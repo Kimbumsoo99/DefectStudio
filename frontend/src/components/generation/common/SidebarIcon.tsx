8ba24d1a8d7c810f37269470730b96a9b3b5ba67
surina125 <jhyunsoo1125@gmail.com>
2024-09-29 11:20:09 +0900

    [#98] fix: output zip 선택 기능 추가, 배치모드 파일 자동 다운, 사이드바 진행상태 아이콘 추가
    Former-commit-id: 6e064f1d9a5b4f7135b892b4c165761f3c08de4e

--- frontend/src/components/generation/common/SidebarIcon.tsx
+++ frontend/src/components/generation/common/SidebarIcon.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9222fa7d2042b2dfcb6aa37470f71cf3805d2ba094817e4beb4a430c58c432f2
-size 1110
+oid sha256:70bfbe41917717965f8417c8d3b409b1cba78bc44016d2dc17538108cf9425a6
+size 1285
