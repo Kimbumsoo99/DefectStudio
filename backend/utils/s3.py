2535d42fe73addf0cbd499891cda227da55a6501
garamgim <garamgim@gmail.com>
2024-09-30 00:21:18 +0900

    [#76] feature: GenerationLog 삭제 시 S3에서 해당 이미지 삭제
    Former-commit-id: 9e7e93e37533b48f7c5ab238669f008c351bc7dd

--- backend/utils/s3.py
+++ backend/utils/s3.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2114bb63042e50be08642266ff052ace1e9b5d681d0459dd0e8528ddca74fe25
-size 3821
+oid sha256:e2d890ed4f75450333030933a41663a56c317dce6a1890ebc5551e2bd25fb751
+size 4450
