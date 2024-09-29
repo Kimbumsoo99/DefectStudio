1410c5cf643fba6488d4be583ffb5abea343d415
surina125 <jhyunsoo1125@gmail.com>
2024-09-29 19:24:11 +0900

    [#98] fix: 배치모드 파일 변환 오류 수정
    Former-commit-id: 993bfbed99f3e09b38354eef6778d5ae0b5c62cb

--- frontend/src/components/generation/layouts/Txt2ImgLayout.tsx
+++ frontend/src/components/generation/layouts/Txt2ImgLayout.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ff32c26c11060c952c7acd2fc54eff47c857a7b1fdef708576e41f8a4fe00121
-size 6857
+oid sha256:80167e5ccc4128640b905511f31bc1bdc38f11b6edc6069338aefc02ba7f3385
+size 6876
