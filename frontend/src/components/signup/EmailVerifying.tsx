24eae65f0e8cb003c8be6d452e5d2e26f1e925d7
SnowyPacks <originpc@naver.com>
2024-09-29 18:01:16 +0900

    [#120] feature: api 연동 및 성공시 회원가입 요청 코드 전송
    Former-commit-id: 390bafeff63ac8a1cb486ee2e80e474bf33f66fe

--- frontend/src/components/signup/EmailVerifying.tsx
+++ frontend/src/components/signup/EmailVerifying.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1a37b96c5e508ec0dec5b9a5ca920cbf15e2a3fe3bc0011ba2b49893c323f06a
-size 4993
+oid sha256:49e59e11b067ea95a495033f73c5cbf45e59f633e5ff573143acce5cf67b18eb
+size 5532
