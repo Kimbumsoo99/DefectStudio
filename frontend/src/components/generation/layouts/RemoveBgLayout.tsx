09894dcf1770402d3cac35708a9c91dfb4a31bb5
surina125 <jhyunsoo1125@gmail.com>
2024-09-29 16:33:30 +0900

    [#98] fix: 배치모드 파일 변환 오류 수정
    Former-commit-id: 9faac60ec6f59b415d0a317d7fec7ba6cbc4d3c7

--- frontend/src/components/generation/layouts/RemoveBgLayout.tsx
+++ frontend/src/components/generation/layouts/RemoveBgLayout.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8bbdf1e485d6ecbdbe9bbaaf8e0b674f1219b9e409a784ed8048709d628fc3cb
-size 6240
+oid sha256:1563b32e516f80cef611ba791cab8d3228f6358fbd9014987352f6fc4ea6a47c
+size 6253
