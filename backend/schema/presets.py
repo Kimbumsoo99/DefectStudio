ecccf2ff1bb559fd21ce015d81e6dc5f59e7ceda
garamgim <garamgim@gmail.com>
2024-09-30 09:47:26 +0900

    [#76] fix: 추론 수 변수명 변경
    Former-commit-id: 0b39a0b5e5dd2a64b7011ebc15f139ec7d7de5a0

--- backend/schema/presets.py
+++ backend/schema/presets.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:80e0ebaca588a1838a7172ae3bfb3d605156ea7ad7dfe746d20549165550b338
-size 1232
+oid sha256:c6393aa41b50b2248fda00725f162138c0d02e90cd5f246cba6f6147d7e989f8
+size 1242
