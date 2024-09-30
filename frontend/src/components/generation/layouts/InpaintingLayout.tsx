b32a63be3b382cbea82d337366c794929e02c964
surina125 <jhyunsoo1125@gmail.com>
2024-09-30 17:45:34 +0900

    [#122] fix: 오타 수정
    Former-commit-id: 1c78bf5138462d53e2ab033325253c0693e6790f

--- frontend/src/components/generation/layouts/InpaintingLayout.tsx
+++ frontend/src/components/generation/layouts/InpaintingLayout.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2e05f57508eb1283b2e4c139475083dd7ccd972274ec6e4d524ba6c13a9ebd97
+oid sha256:a2d942cfd512ad2d5954387da5ba5c2bc94673110343b5f55ac768dd75691f42
 size 9679
--- frontend/src/components/generation/layouts/Txt2ImgLayout.tsx
+++ frontend/src/components/generation/layouts/Txt2ImgLayout.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b2f4e5187146461166d395f404c7b9c9c1df62c10fd4f6d6b09d2fb3a95be56b
+oid sha256:aad26bc949951d9317b40fc0a1f4066d19a707645ea804ac81525763bf351407
 size 4502
