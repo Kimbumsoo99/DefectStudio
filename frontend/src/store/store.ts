17b2f57b083907f047cc9ac00cf81d8a7cf04329
surina125 <jhyunsoo1125@gmail.com>
2024-10-01 18:53:35 +0900

    [#122] fix: 책갈피 저장, zip/일반다운로드 기능 추가
    - 책갈피 저장된 폴더들은 홈화면에 띄워짐
    
    
    Former-commit-id: 53e5b7e89f0660953deaa0a441b9c4d8290d11e3

--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:967247675366e83bef4783c62f71af4dd7a87dcb8a4b0bd8d383a8b54558e64e
-size 1111
+oid sha256:22705bb7140731e771042262ccfba8a691c91fe1db39896708bb611fa6fea294
+size 1195
