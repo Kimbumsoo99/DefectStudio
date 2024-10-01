69df3fabbabd3cebc25a62ea6230be85f94caa80
garamgim <garamgim@gmail.com>
2024-10-01 17:40:17 +0900

    [#76] fix: total_image 계산 오류 수정
    Former-commit-id: 624de4313318e36da245ea92528ad5d3307c8914

--- ai/utils/tasks.py
+++ ai/utils/tasks.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a3b0251d0736b7e054997f5d3ba97a42cb90498bd7b8cdda821c7d7645bd0902
-size 17242
+oid sha256:9bafe11491a4872d72eef39f0fe73085926f1d4f42f53c3959879da9154fcd1d
+size 17265
