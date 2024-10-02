bc82bdefd2244e6c78fa80b667339be51f4744ce
Kimbumsoo99 <show7441@naver.com>
2024-10-02 11:40:09 +0900

    [#102] fix: Training Issue 해결
    - num_class_images 조정
    - 기타 파라미터 수정
    
    
    Former-commit-id: bc3634f02c11169325f5bd451a2a290c322cf52f

--- ai/api/routes/training/dreambooth.py
+++ ai/api/routes/training/dreambooth.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:01701fbdccfc79312147869fa04c24239dd23938fe7329097e0e73140cffcaf9
-size 11036
+oid sha256:fbdfc9b6490b4d0318e94665d6474be2f603161359a381f2946595b635fa7bbe
+size 11384
