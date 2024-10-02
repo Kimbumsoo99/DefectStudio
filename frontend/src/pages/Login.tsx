c1c025af5e1c3bd794a6f9e9b096f7269d58767b
SnowySparks <originpc@naver.com>
2024-10-02 12:44:21 +0900

    [#125] feature: 배경화면 추가 및 다크모드에 대한 css설정
    Former-commit-id: be71c480545b20c2ca9b65c3fa706e931b32d454

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e724b62a6c8bbfc36e4a1406c8638ac5f08a55989dd6c4d35daebb98b1bc6f1b
-size 1197
+oid sha256:c9b88a94f3acd8350b0d1fc152eff0b52e0500117882f073b65c9dccf3564729
+size 1339
