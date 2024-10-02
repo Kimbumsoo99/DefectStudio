a21d493be14af492731dfffb24f5849014b2be61
surina125 <jhyunsoo1125@gmail.com>
2024-10-02 16:35:28 +0900

    [#123] fix: settings maxImgsNum 추가
    Former-commit-id: dfcb516de3acee082cc8911ba1b2d98c9519914f

--- frontend/src/api/settings.ts
+++ frontend/src/api/settings.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d066a52a8fedb9b3b0cfe3a7e5dce5fc6fe9426db50b68292bb492a5eaced8b9
-size 1752
+oid sha256:5baed05fca00f02b061d78ff5b35c2b00aeba25f354353f7f5bfc2f7eda5fbaf
+size 1957
