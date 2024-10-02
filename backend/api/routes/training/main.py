3bc6289fe4d82b53294ee662f14c99710a8660b5
garamgim <garamgim@gmail.com>
2024-10-02 21:23:15 +0900

    [#81] feature: Training API Celery 적용  #S11P21S001-509 #in-progress
    Former-commit-id: 9914e0f70f87dde3a2e09c9fbec54be98062eaf2

--- backend/api/routes/training/main.py
+++ backend/api/routes/training/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:27a6f8aeef78f23378e6e3127e06a9e17da5e40d46180aeae933d6b2e4f52f46
-size 185
+oid sha256:b3dbb3751d85f18187d9aa78329e7f8500a78e1e51d73bc5e49dd4753e64b8b0
+size 1367
