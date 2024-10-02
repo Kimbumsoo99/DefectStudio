27ce3f0b016d2e64f068e67e75cf4e38e74f89ec
surina125 <jhyunsoo1125@gmail.com>
2024-10-02 09:45:16 +0900

    [#122] fix: 책갈피 기능, 일반 다운/zip다운 추가
    Former-commit-id: c5befe4999bd8d0aea434343831b7641c2bbaba1

--- frontend/src/components/history/ImageFolderDetail.tsx
+++ frontend/src/components/history/ImageFolderDetail.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a0826c1da3e74777b76034d589f6dc2c620d487cc8adf97dca158c454fa4a163
-size 7396
+oid sha256:e87cd5ddb90eadbd4653c0f040762e04a913d429fac52e57279a056ed50706ca
+size 8173
