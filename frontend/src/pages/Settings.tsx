96a7446cca6fe6ade136a94def5364e1056f3bac
surina125 <jhyunsoo1125@gmail.com>
2024-10-02 15:19:25 +0900

    [#123] fix: settings: gpu Device 상태관리 추가
    Former-commit-id: 1a8e5617f65e3a642a1427b6633ebb35d81be2b9

--- frontend/src/pages/Settings.tsx
+++ frontend/src/pages/Settings.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d381867c966fa9a31033af41e325b012ba7ba570997c99e7b0540ea3e4147dd7
-size 9554
+oid sha256:71e94792777bfd94ae8804f311442bc353bb85f000d250ba70916fac1a23283b
+size 9251
