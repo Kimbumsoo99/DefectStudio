ae78d5e2f8da86b7900d17c2e3b8d3646baf2717
garamgim <garamgim@gmail.com>
2024-10-02 21:23:15 +0900

    [#81] feature: Training API Celery 적용  #S11P21S001-509 #in-progress
    Former-commit-id: 39cbe236eab95248004c1394d5db6a3e91fff5dc

--- backend/api/routes/training/main.py
+++ backend/api/routes/training/main.py
@@ -1,10 +1,3 @@
-from fastapi import APIRouter
-
-from api.routes.training import dreambooth
-
-router = APIRouter(
-    prefix="/training",
-    tags=["training"]
-)
-
-router.include_router(dreambooth.router)
+version https://git-lfs.github.com/spec/v1
+oid sha256:b3dbb3751d85f18187d9aa78329e7f8500a78e1e51d73bc5e49dd4753e64b8b0
+size 1367
