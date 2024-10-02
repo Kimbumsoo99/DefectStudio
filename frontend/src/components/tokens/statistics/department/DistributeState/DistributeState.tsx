e97173c476c0bfcd9e62d1ccbfb01d163a20b4b4
SnowySparks <originpc@naver.com>
2024-10-02 14:52:21 +0900

    [#125] fix :Department Distribution State 시간순으로 배열
    Former-commit-id: 9728445b10421e6837bc5f8d0581f1be41741bb1

--- frontend/src/components/tokens/statistics/department/DistributeState/DistributeState.tsx
+++ frontend/src/components/tokens/statistics/department/DistributeState/DistributeState.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4dda069cd27d10cd3a8032b0c92c562afbcd015ab4350c5090e7513fbd613091
-size 1448
+oid sha256:466ade01812f5af93d0ae0da2855aace71ad42250f1af4c5a6021c8bfff9dc2f
+size 1548
