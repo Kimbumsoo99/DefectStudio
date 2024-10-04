a630486ff7b2f5c14854cd642c50718822926eb4
SnowySparks <originpc@naver.com>
2024-10-04 11:25:42 +0900

    [#128] chore: 미적용된 Tooltip 폰트 옵션 반영
    Former-commit-id: d58b80e22df181ea930363f64c316a86b8178d7a

--- frontend/src/components/tokens/statistics/department/ToolUsage/DepartmentToolUsageGraph.tsx
+++ frontend/src/components/tokens/statistics/department/ToolUsage/DepartmentToolUsageGraph.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ea922b2ea50524ae135988a44b48c6b7aa0cff5d91c2a39faa01c5e720b02280
-size 1924
+oid sha256:b61a9f58a991a66e62f471edaeedd7f86f5637dc9a5aefc2af79c5df2e0260e7
+size 2465
