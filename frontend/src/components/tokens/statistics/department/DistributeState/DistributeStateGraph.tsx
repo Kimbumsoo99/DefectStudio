f474d2c33821ed982533ed0bb10a8fd39954e63c
SnowySparks <originpc@naver.com>
2024-10-04 11:28:50 +0900

    [#128] feature : Y축 라벨 추가
    Former-commit-id: 9451de15899303777d13dcae6654042c24aeeca5

--- frontend/src/components/tokens/statistics/department/DistributeState/DistributeStateGraph.tsx
+++ frontend/src/components/tokens/statistics/department/DistributeState/DistributeStateGraph.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4fe6d2bdf3484a0bd6c0128fad57ffec3ed8aaed5c62f36daac058a8bdd96946
-size 2855
+oid sha256:965fec9e7fea73eebaf74c6ed475dc29845309200a9abde052e104fec4caa58b
+size 3054
