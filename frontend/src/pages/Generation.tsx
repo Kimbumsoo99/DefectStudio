8460b5ff4c000ff0b4c27e70c9c3365e37f3aa87
SnowySparks <originpc@naver.com>
2024-10-04 14:51:52 +0900

    [#130] fix: 토큰 사용 업데이트 query 수정
    Former-commit-id: 3e35c01d0031e26b8ee0be9d32309e8901883b49

--- frontend/src/pages/Generation.tsx
+++ frontend/src/pages/Generation.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2db5109bb71ea772904e735d39d05d6b85ac8ceedf9093e7e7225354c343e2b6
-size 11797
+oid sha256:21ac314794880865bacda849066af3df6141cb9faf3d804d21d28f6a7d520017
+size 11870
