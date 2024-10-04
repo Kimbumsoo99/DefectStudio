399485c04942f61a3bd307cde4c0e8aff6bf3bd2
SnowySparks <originpc@naver.com>
2024-10-04 15:54:36 +0900

    [#130] feature : 프로그램 실행시 Loading Indicator 표시
    Former-commit-id: 2c57c6676f9fefe439961e52b08de944b6786081

--- frontend/electron/main.ts
+++ frontend/electron/main.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:139b0058bbf50fb77226db96187bc8c674870f808fbd091a4697f8fc15b8dc43
-size 6756
+oid sha256:ab6a053a702a2b58fc374dab6c557bf8e41b26a45bd3c91aae7057341885c493
+size 9139
