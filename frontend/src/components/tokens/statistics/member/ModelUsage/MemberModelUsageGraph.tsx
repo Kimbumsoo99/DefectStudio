a50454d4731db2a35f32c6935740fe33a8d10b51
SnowySparks <originpc@naver.com>
2024-10-04 11:12:04 +0900

    [#128] feature: Tooltip 표시변경 및 폰트 변경
    Former-commit-id: 7036eee39015055c9bca4a78a25ed60683274585

--- frontend/src/components/tokens/statistics/member/ModelUsage/MemberModelUsageGraph.tsx
+++ frontend/src/components/tokens/statistics/member/ModelUsage/MemberModelUsageGraph.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:da13925d18891ff032ea00ada5b585540e81175928cd1093415af893b3ccbf37
-size 1920
+oid sha256:6bf6b82b04413be7e743149e51caba1579f6073225b8545e67b5e6c59387b6b2
+size 2461
