d26296f4c8e4de4a159fb710b43d015d77e36f9a
SnowySparks <originpc@naver.com>
2024-10-04 15:55:20 +0900

    [#130] docs: 주석추가
    Former-commit-id: 4858533d8934540f10ea9bcf6792b8c158b5da4c

--- frontend/electron/main.ts
+++ frontend/electron/main.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ab6a053a702a2b58fc374dab6c557bf8e41b26a45bd3c91aae7057341885c493
-size 9139
+oid sha256:b7d5acf6c0396ac6e5323fce627a61983e01842f993aa72f1cda57fb1b6d2871
+size 9221
