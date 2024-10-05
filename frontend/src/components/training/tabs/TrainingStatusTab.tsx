91cbcb4e96a9ed0ca19c24f4baff9d5cf24b646c
surina125 <jhyunsoo1125@gmail.com>
2024-10-06 00:50:01 +0900

    [#107] fix: training progress 추가
    Former-commit-id: 185b30c1e3293d16d47ca37a731ccac41ad17e44

--- frontend/src/components/training/tabs/TrainingStatusTab.tsx
+++ frontend/src/components/training/tabs/TrainingStatusTab.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:df178cea70e530b73b413f60e3ab10b14a81ce334eec550a560b6cf49afa94eb
-size 5843
+oid sha256:06e634f1ac397d6d8a7e056603c1dec6ffe76d21aea05bebadeeba24ae61a612
+size 5503
