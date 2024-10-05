ccc63823d0c5e1cd699978de479d16114da0c104
surina125 <jhyunsoo1125@gmail.com>
2024-10-06 02:10:56 +0900

    [#107] fix: taskId 상태 확인 3초뒤로 수정
    Former-commit-id: 85e18a5502bea218d806ae18e66b2fcb5c3b2d43

--- frontend/src/api/training.ts
+++ frontend/src/api/training.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3b6ee5f9662cb70de761bcf60c78fef3cac80ba26c82564e8d7f300891ccf983
-size 1610
+oid sha256:b8a93250a5bd0115cd50459efb382f5f4d64dc94c4438cdf014cbb9809749361
+size 1619
