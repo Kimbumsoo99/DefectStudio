c14ee8dc7e19194b39411af5ab8c2cb230217c5e
surina125 <jhyunsoo1125@gmail.com>
2024-10-05 14:30:39 +0900

    [#107] feature: training graph
    Former-commit-id: a61c06e4dee3cff0f4cba0a184b64d57dc498010

--- frontend/src/pages/Training.tsx
+++ frontend/src/pages/Training.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:594f6d578b8666b2cea1e33474da944575ff2cb18db63cbcc75d7786c8ad8b99
-size 1428
+oid sha256:0699c71a97bde969e95bb20b82d27a35292bf6b49fd67959db439e29330edf28
+size 1398
