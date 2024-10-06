0d68777bc2869644151825053f7485655f39256d
JaeHunJang <wuddlaa@gmail.com>
2024-10-07 02:52:57 +0900

    [#90] feature: Image Generation Inpainting
    #S11P21S001-516
    
    
    Former-commit-id: 5cef1b60681bb76921be3854a212e47337340f40

---
+++ ai/classification/gen_data/bottle_inpaint2.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ac8d03901891115e9a19a2ba990ef240ead4266ae8bc98f644901e88f2ca7a06
+size 4260
---
+++ ai/classification/gen_data/leather_inpaint.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6695594b4eb95772807b5d673c6ddf7c9bc6d33ba2b01e1521a3932c36d1692c
+size 4127
