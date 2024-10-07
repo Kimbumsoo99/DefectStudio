e8ec42fdf96e18171744c0c939f59983b9199e1b
박수진 <tnwlssla20@naver.com>
2024-10-08 08:34:38 +0900

    [#137] fix: 모델 없는 경우 에러 처리
    Former-commit-id: a20fa207b536ad86cdef01f6d9ee8c2fea07c09e

--- ai/api/routes/model/model.py
+++ ai/api/routes/model/model.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1d02f539367fb1ae987eb0644e3f9eefa0d093c4aff7ee0155ed3bb164987cc1
-size 1139
+oid sha256:236e5e4c2aa616d77aa0d01ca6ed12edfaefc2a7e26d51ac8c53b17a543845f2
+size 1332
--- ai/workers/tasks/model.py
+++ ai/workers/tasks/model.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2e4b2aafb1bc4b7a520ded0a25c0249b1aaaba3efb983c51e029c93b5c5f9f2b
-size 835
+oid sha256:adb6e2fcf811cfea755c90247e39c60205f2303ac8097d3ce65e3769ed2e9a26
+size 752
