f0bde5dc8ff4772fa958d503cf4c4da5774c159a
surina125 <jhyunsoo1125@gmail.com>
2024-10-08 04:11:59 +0900

    [#98] fix: masking modal brush 작동 중 튀어나옴 오류 수정
    Former-commit-id: 27b0a7f66ab3704323911eb7784cc2182be2058f

--- frontend/src/components/generation/masking/MaskingModal.tsx
+++ frontend/src/components/generation/masking/MaskingModal.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6837a776d9e4ad2da10d0bc4dd0fbfd87c5a63979b643bc24edf59474d0a4552
-size 26083
+oid sha256:2f5d03853dd686da4b137a123303825bcddb2c54211caec792136c28c45c06b4
+size 26117
