6294dac72473051208fb6f1e2dbffcb3dfca1d60
garamgim <garamgim@gmail.com>
2024-10-07 09:59:08 +0900

    [#132] refactor: Celery 디렉터리 구조 변경
    Former-commit-id: 9c41795c400a5191014f4b02d1140b7a7d8aca86

--- ai/api/routes/generation/cleanup.py
+++ ai/api/routes/generation/cleanup.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d7a69d957088c5963acd654f861b4e80c6a996d2f1dbf7115cf9715b4816dcec
-size 884
+oid sha256:3289e9941467d71dfa99ea65b1b2379aaa0f320ffff9615d43410c891cc06605
+size 896
--- ai/utils/tasks.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:8d8a14404a18bd9527dea05426536f524211b96e6c1fb194040a978973bdc53d
-size 17742
