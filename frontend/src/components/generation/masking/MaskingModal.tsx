6b551e2cac4287135db3e327665cf3a9bb5e622a
surina125 <jhyunsoo1125@gmail.com>
2024-10-08 02:42:08 +0900

    [#98] fix: masking modal 이미지 로드 오류 해결
    Former-commit-id: a37d5624b1af489fa95abafbc7d6217761fcfa2b

--- frontend/src/components/generation/masking/MaskingModal.tsx
+++ frontend/src/components/generation/masking/MaskingModal.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:801d142394a48c0e34fb6f2d33eaae273432762864bf289409f7a0b2c1587114
-size 26080
+oid sha256:6837a776d9e4ad2da10d0bc4dd0fbfd87c5a63979b643bc24edf59474d0a4552
+size 26083
