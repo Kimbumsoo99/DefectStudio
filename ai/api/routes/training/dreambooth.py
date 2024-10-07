bb11089849910240b8c998c6957019ea4df1fa2d
garamgim <garamgim@gmail.com>
2024-10-07 09:59:08 +0900

    [#132] refactor: CeCelery 디렉터리 구조 변경
    Former-commit-id: fc62d570fe92f7b8d036dff868316d7fb500a35e

--- ai/api/routes/training/dreambooth.py
+++ ai/api/routes/training/dreambooth.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:193442068339280283a9a9300cd090b6ce3ca3b5d68be92d21d4b901a8db4613
-size 11474
+oid sha256:e08bd3d7de4e898217815abc2c20b5dca3dfc6af5b78998a7b9508c0fd764324
+size 11484
