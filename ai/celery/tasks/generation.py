745cab199bd4969d6f58cc864fd4400d83c97e40
garamgim <garamgim@gmail.com>
2024-10-07 11:31:42 +0900

    [#132] fix: 디렉토리 이름 workers로 변경
    Former-commit-id: edd13732dcd9ef5dda8b8d9dc82b620052933dd7

--- ai/celery/tasks/generation.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:89836adb111abcb837bc0ae9c61a362bf175fee12ce80833a2d01eba0ec9674f
-size 17273
---
+++ ai/workers/tasks/generation.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a6ec2c4d089201977a141d9f3c0a9a8d1cdaca5511f8123f049d99ab5a23d307
+size 17274
