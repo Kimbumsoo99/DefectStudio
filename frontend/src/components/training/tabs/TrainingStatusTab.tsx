683a98533e6c34b2ed460226f14c3836a89c4fc3
surina125 <jhyunsoo1125@gmail.com>
2024-10-07 15:03:01 +0900

    [#107] fix: training 페이지 ui 수정
    Former-commit-id: 24397aa245de2f6c94c1ff0f17e7a3e4e86cfa84

--- frontend/src/components/training/tabs/TrainingStatusTab.tsx
+++ frontend/src/components/training/tabs/TrainingStatusTab.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:19691f15c911d29e3b814deffc8b76fba48ed1a60ca70e9c739bf732ea035c59
-size 5513
+oid sha256:984d64cdade43aee1ae71b27fe8030ea5eaaeda8410ded66488235ec0f4516ad
+size 7842
