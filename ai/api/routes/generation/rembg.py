dd7718804645b0fe5465e6e23127f5629a660990
garamgim <garamgim@gmail.com>
2024-10-07 11:31:42 +0900

    [#132] fix: 디렉토리 이름 workers로 변경
    Former-commit-id: 55028bae1191a4a68e72790ed5cd3b31281591dd

--- ai/api/routes/generation/rembg.py
+++ ai/api/routes/generation/rembg.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4279e977553cf0344ddb2a3c773e05383657a3a0a1755d2fc8daa0d364c69935
-size 730
+oid sha256:32ccdaa86b734e63fe2d90437e5918fc79e5ab37f25da770ef8273d979ca5d48
+size 731
---
+++ ai/workers/tasks/training.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7f62a4a9ca5ffd32d4af4515a8a93178b6fc919186abf04a0a6d17fd67e2e480
+size 559
