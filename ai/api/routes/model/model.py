ce671c83d630ca44bea57460da365be5148fb4af
박수진 <tnwlssla20@naver.com>
2024-10-08 01:24:29 +0900

    [#133] refactor: 모델 경로 확인 로직 위치 수정
    Former-commit-id: 75968bbebba8b01a2865241d1c789d2cc1fe714e

--- ai/api/routes/model/model.py
+++ ai/api/routes/model/model.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0b10f45c6ecefd8369eb9597bf88c681b424e3fdf7dfa1fefd8365846ce8c260
-size 1254
+oid sha256:1d02f539367fb1ae987eb0644e3f9eefa0d093c4aff7ee0155ed3bb164987cc1
+size 1139
