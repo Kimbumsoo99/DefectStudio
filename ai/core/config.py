e15379233f30a56e2da1f558d4c0f4f8f5f2d2da
박수진 <tnwlssla20@naver.com>
2024-10-08 01:22:30 +0900

    [#133] feature: download 임시 저장 경로 설정
    Former-commit-id: 3b9f32b10d701f60eb8714f6df6e111bd5bf79db

--- ai/core/config.py
+++ ai/core/config.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dedcca0a269343aaaaa077ae538a753abaec099929a8e5b6025b87e2d5255399
-size 474
+oid sha256:5ce489c78072000e46cf1de3835694bcff5a6ed4a71e939d0f3d2b245ad92e94
+size 501
