0d566978295c2987a1f9d1bc1c108d94ddcae852
garamgim <garamgim@gmail.com>
2024-10-07 11:22:50 +0900

    [#134] TTI S3 업로드로 변경
    Former-commit-id: 25cd4300cdae7d84ed3d6e6e322351b32a4614c3

--- backend/api/routes/training/main.py
+++ backend/api/routes/training/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a9bb76454fd24fe08c0431afcf023157c467c628920afca0b1702eef777b794f
-size 1373
+oid sha256:d6e4fee1e0d54e5eab42e0d57fd595c5059901c5b6c2a8059e7f7812361ef44a
+size 1153
