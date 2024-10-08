edc7090ba7f79dfddc85f9c89471d0ee7fc33639
SnowySparks <originpc@naver.com>
2024-10-08 09:29:00 +0900

    [#138] fix: Settings UI 가시성 업그레이드. 버튼 선택 바로 반영 안되는 문제 픽스. 성공/실패에 대한 알림표시
    Former-commit-id: 9bfbfbafcd57d3f1dc3bc1b3ec5e816802ef6055

--- frontend/src/types/settings.ts
+++ frontend/src/types/settings.ts
@@ -1,3 +1,16 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:737c2e35c33a863ea50031a52bb88577d15f6394efd4eeed227c2f9ca00f79be
-size 308
+export interface GpuInfoType {
+  'GPU num': number;
+  'GPU name': string;
+  'Total memory (MB)': number;
+  'Free memory (MB)': number;
+  'Used memory (MB)': number;
+  'Free memory (%)': number;
+  'Used memory (%)': number;
+}
+
+export interface gpuInfoResponse {
+  data: {
+    gpu_info: GpuInfoType[];
+  };
+}
+
--- frontend/src/utils/snakeToCamel.ts
+++ frontend/src/utils/snakeToCamel.ts
@@ -1,3 +1,9 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:6cca19fbf33d2f6eb8b527be245e4816c94c52214e86ab14827d2d4a9e135f02
-size 388
+// snake_case 문자열을 camelCase로 변환하는 utility Type
+export type SnakeToCamelString<S extends string> = S extends `${infer T}_${infer U}`
+  ? `${T}${Capitalize<SnakeToCamelString<U>>}`
+  : S;
+
+// 객체의 키를 snake_case에서 camelCase로 변환하는 Mapped Type
+export type SnakeToCamel<T> = {
+  [K in keyof T as K extends string ? SnakeToCamelString<K> : K]: T[K];
+};
