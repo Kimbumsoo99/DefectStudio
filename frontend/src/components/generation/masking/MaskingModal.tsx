c6a59a96edf67957cc82035d38fe2625c3eb02cb
surina125 <jhyunsoo1125@gmail.com>
2024-10-08 09:50:09 +0900

    [#98] fix: tab이동 오류 해결
    Former-commit-id: d59846b835f0a1b631e18967b465976f3dfaf547

--- frontend/src/components/generation/masking/MaskingModal.tsx
+++ frontend/src/components/generation/masking/MaskingModal.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8b6cf5c11381123d0e3f397ac1b3d1d5e7e54dc73450b667ef3f7fc22c11d3c0
+oid sha256:de5895b1e426b06c03e4ab42168ac023ae21ce1bd7a157ef07a43424e2098850
 size 25635
