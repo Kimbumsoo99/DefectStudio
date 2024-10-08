112604521485a56632242b9ccba4d3e053d6f4e2
garamgim <garamgim@gmail.com>
2024-10-08 22:35:47 +0900

    [#140] refactor: training api returncode 반환
    Former-commit-id: 9260ac03da790cad27c683d3e17ee8f1d8ed1141

--- ai/schema.py
+++ ai/schema.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dbf5705d9c8be8c617f124f3454c96caa314c95a267adffcdce601f9f0b6753b
-size 714
+oid sha256:6eb7eba829c2152565af0a2e87792ec0b8f6295256580b9423dc97a44b4ddc56
+size 752
--- ai/workers/tasks/training.py
+++ ai/workers/tasks/training.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:35f7a9f0484a86981e52c7fe72ac255ae25421e660a805b76a1df2a37f5b223b
-size 704
+oid sha256:7692afbdc634c44e56a137e303a0ba7ac0fb818d856754fc67e9c98e21872a9b
+size 765
