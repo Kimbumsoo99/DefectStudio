0e366014778b0f3ed4854b81920e455d3329e41e
surina125 <jhyunsoo1125@gmail.com>
2024-10-08 13:11:18 +0900

    [#98] fix: clip loading추가
    Former-commit-id: d3132ffc9615f5aea317c90d70f8e58904ef0a11

--- frontend/src/components/generation/layouts/Img2ImgLayout.tsx
+++ frontend/src/components/generation/layouts/Img2ImgLayout.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8105b363fcc06437c838a9701ac8fbb5c9de67cf2c147a410baeaf97820eff1b
-size 8427
+oid sha256:252b7222997c37f368eac95e8eb0be62b52e1c7151446f2bf294e81558139c77
+size 8381
