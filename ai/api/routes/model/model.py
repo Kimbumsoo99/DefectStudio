25bdd74b747a2613283960048e839c3c77cfd5f3
garamgim <garamgim@gmail.com>
2024-10-08 10:03:27 +0900

    [#137] fix: model task 응답 수정
    Former-commit-id: 2bfb4dd7005ec204179c9f8747b0481cbf510aa1

--- ai/api/routes/model/model.py
+++ ai/api/routes/model/model.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:236e5e4c2aa616d77aa0d01ca6ed12edfaefc2a7e26d51ac8c53b17a543845f2
-size 1332
+oid sha256:37c8d3e46ba9544ea0cca2b5dd4363efc48af280a638d5eacfd40654f9bc5ad4
+size 1443
