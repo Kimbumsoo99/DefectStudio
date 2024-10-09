fe6fd0d4c5ea60f31cb2d905ed7b56fc326bb159
surina125 <jhyunsoo1125@gmail.com>
2024-10-09 22:34:06 +0900

    [#107] fix: 시간 초 수정
    Former-commit-id: 894fb3a4c0e317ae8a65693e222e6bc542e2b2c1

--- frontend/src/components/training/common/TrainingButton.tsx
+++ frontend/src/components/training/common/TrainingButton.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4f47f8af48b443997922c09008b76e9338270ab3560d762eb43308cecf1b7f61
+oid sha256:ceba23196295f2b823e00ad195c44ac3a2e2d4f24244cfb7b7bcdea92bb0e92a
 size 5977
--- frontend/src/components/training/tabs/TrainingStatusTab.tsx
+++ frontend/src/components/training/tabs/TrainingStatusTab.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:82409e7b1c65ac637c44b1e7281a53e17d1e6857871fbe2e9e79b0859bc29915
-size 8444
+oid sha256:ec856b7175b3b212c54620cc381d25f51cee73be0fe5c2fd5b4f2cea934434d3
+size 8379
