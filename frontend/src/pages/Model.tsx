2b9d0ae34c078519312e2d43375e92371bd4fbff
surina125 <jhyunsoo1125@gmail.com>
2024-10-09 14:12:21 +0900

    [#107] fix: member_id 반영해서 model list불러오기 수정
    Former-commit-id: 5338f8fc6bb704fc3759d79ca7067e032380da1e

--- frontend/src/pages/Model.tsx
+++ frontend/src/pages/Model.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1ac3f0ce8c03dd17d55179509e55b519498cd8db5e42e055a6d86f2e0ec853a1
-size 4264
+oid sha256:ba1c2fe6c2280ef0e96fcaedb4b9af505d5793076ad3378c66efdfabb487616f
+size 4322
