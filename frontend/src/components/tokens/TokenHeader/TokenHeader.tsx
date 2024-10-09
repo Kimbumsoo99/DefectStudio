175bd3a2ecf6db0b2e9844d0601f798d3bf661f1
surina125 <jhyunsoo1125@gmail.com>
2024-10-09 20:14:50 +0900

    [#107] fix: training test
    Former-commit-id: 6556ead283dc60b31834ba6f1debf1af4915d2f6

--- frontend/src/components/tokens/TokenHeader/TokenHeader.tsx
+++ frontend/src/components/tokens/TokenHeader/TokenHeader.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:671aa6c9a80fe42fcf7f450607d052747fd21e0b48e984cd1dc76678b1267f0b
-size 910
+oid sha256:48e436a9527fc8b538f053d9a8135a826ea5f8687d60e83e030e565bdaa163d0
+size 913
