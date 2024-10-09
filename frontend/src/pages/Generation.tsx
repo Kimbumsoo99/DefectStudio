e4e669a758ceada1436dc76b93f703f1009f5ae0
surina125 <jhyunsoo1125@gmail.com>
2024-10-09 15:49:46 +0900

    [#107] fix: generation 세부 수정
    Former-commit-id: 91e9a00d240f35a23770e6dd3c2f9694334728f1

--- frontend/src/pages/Generation.tsx
+++ frontend/src/pages/Generation.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:272db93d9a79dede3f6d3ec34af579c3b9c0a45b0fce871e5cbc64ad0453ac7c
-size 11883
+oid sha256:8e4d52caeccb1e8b016a9d382f985aec03bd28fd6921600dbdda9d3a5f9a0dbd
+size 12080
