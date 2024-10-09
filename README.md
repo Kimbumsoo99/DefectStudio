30e79ddc3afe1e3aea122c003df067effa34dcc0
SnowyPacks <originpc@naver.com>
2024-10-09 15:09:52 +0900

    [#136] docs: README.md AI 추가
    Former-commit-id: 21dee9714d75db778de4e97e0b2a1c6768dfd33c

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d42d8e358f688f7724b0d4e38f7cd1706c81f177442ab9d0376f1db0c8a273dc
-size 6122
+oid sha256:a8ef145b3b323e492821c308c7d12bc660dbe09af521be1369b1b73478b272aa
+size 6327
--- ai/core/config.py
+++ ai/core/config.py
@@ -1,3 +1,21 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:4ced3cfc011059d7c3648a7178b5ca8464adbb4038c0059353385a79ec54c8ec
-size 440
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env", env_ignore_empty=True, extra="ignore"
+    )
+
+    # ENVIRONMENT
+    OUTPUT_DIR: str
+    DIFFUSERS_TRAIN_PATH: str
+    BASE_MODEL_NAME: str
+
+    # LOGGER
+    SENTRY_DSN: str
+
+    # REDIS
+    REDIS_HOST: str
+    REDIS_PORT: str
+
+# 환경 변수 로드 및 검증
+settings = Settings()
