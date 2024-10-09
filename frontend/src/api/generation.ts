8f473b2b3db74de113fc1c2dc1d6a883bc410cf7
surina125 <jhyunsoo1125@gmail.com>
2024-10-09 17:34:39 +0900

    [#107] fix: generated 이미지 확대 기능 추가
    Former-commit-id: 4f68cac68b847d18d340e7399ac23ff6d2ecd0cb

--- frontend/src/api/generation.ts
+++ frontend/src/api/generation.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ec99360e5cc2b713623dec30f713bfa15a3129bfbc7d43d9f7a1056a41ff91c6
-size 8442
+oid sha256:e15612df47aeca4e5827b82ef5a4481c095d34a1d2fd02eae0f0d600287b2648
+size 8473
