ebfbb3ddb7398930f95612546d193e500cd78fa4
garamgim <garamgim@gmail.com>
2024-10-10 12:45:28 +0900

    [#140] fix: csv 파일 미생성 예외 처리
    Former-commit-id: 1d80df74e436ca35761b68802e8cd18404ef2c00

--- ai/core/config.py
+++ ai/core/config.py
@@ -1,3 +1,23 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:5ce489c78072000e46cf1de3835694bcff5a6ed4a71e939d0f3d2b245ad92e94
-size 501
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env", env_ignore_empty=True, extra="ignore"
+    )
+
+    # ENVIRONMENT
+    DOWNLOAD_TEMP_DIR: str
+    OUTPUT_DIR: str
+    DIFFUSERS_TRAIN_PATH: str
+    BASE_MODEL_NAME: str
+    MULTI_CONCEPT_TRAIN_PATH: str
+
+    # LOGGER
+    SENTRY_DSN: str
+
+    # REDIS
+    REDIS_HOST: str
+    REDIS_PORT: str
+
+# 환경 변수 로드 및 검증
+settings = Settings()
