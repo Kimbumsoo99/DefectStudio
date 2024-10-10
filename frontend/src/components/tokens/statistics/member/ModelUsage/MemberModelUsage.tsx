14be1b88c1774c4417d1578dfd4d3250f3779abe
SnowySparks <originpc@naver.com>
2024-10-10 16:07:15 +0900

    HOTFIX : 오름/내림차순 설정
    Former-commit-id: 4a01b3dff96ea94e3fcd674a9838db53648e6fd7

--- frontend/src/components/tokens/statistics/member/ModelUsage/MemberModelUsage.tsx
+++ frontend/src/components/tokens/statistics/member/ModelUsage/MemberModelUsage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3ce3a0abdb8f276d421a9b2400b37e08f7d78de648db203764921793b483cc0f
-size 1462
+oid sha256:12e7ac9f74e16c62469eb2cc6db8c281cd27c693012b89af509c72212a2dedc0
+size 1518
