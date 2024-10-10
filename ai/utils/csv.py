1b95d4cffd1c4194d125c1bc76ca96a06149d65e
garamgim <garamgim@gmail.com>
2024-10-10 12:45:28 +0900

    [#140] fix: csv 파일 미생성 예외 처리
    Former-commit-id: 87f7287081656e9ea8f10fd4632bb5f610b96ac4

--- ai/utils/csv.py
+++ ai/utils/csv.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bfcab379099e74264810a0d43a9b447a7d0cb9e7471a85c8b48e0262e3d60bef
-size 1033
+oid sha256:f5d44f3ac02ed8daef7d9ea313b57d4aee8e0bb84618575e04cb4d952bcfc400
+size 1110
