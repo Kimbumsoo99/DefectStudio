38fa84ff25668d576464bff15f639210331ceadf
SnowySparks <originpc@naver.com>
2024-10-10 10:50:49 +0900

    HOTFIX : 통계 데이터 중복 부분 fix 및 hover mode 긴급채치
    Former-commit-id: 0b3d21af0b9d7981504b877ff7247c8a550c2b92

--- frontend/src/components/tokens/statistics/department/DistributeState/DistributeState.tsx
+++ frontend/src/components/tokens/statistics/department/DistributeState/DistributeState.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b6b0451d11e537554467863041d944b1e130e0b6a18916d9c99c0985bf3e0a7f
-size 1461
+oid sha256:dec88bdf3bc52165433c3dcabea9766a378c8e14dc70523f6284762bb15d7938
+size 1735
