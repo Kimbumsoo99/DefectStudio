c580a4e4e62d668f0f9572e7e5e321195d1defdc
SnowySparks <originpc@naver.com>
2024-10-10 09:16:32 +0900

    HOTFIX : mini_profile css fix
    Former-commit-id: c77c7a71b18872057a3358749512a740d1d49cc3

--- frontend/src/components/profile/MiniProfile.tsx
+++ frontend/src/components/profile/MiniProfile.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3767d762e4207e6083810484a11bf92d2e0506b0095025bc0af76ccfa6c1a6cc
-size 2083
+oid sha256:6f36dce79433fccd668b908692cfeb295147492f86876f01a8bfbfa1f27ab2ba
+size 2086
