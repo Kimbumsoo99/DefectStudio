f0b5492b2c9bfa9aee20f5be646d06651886de71
garamgim <garamgim@gmail.com>
2024-10-11 10:04:18 +0900

    HOTFIX: Sentry 삭제
    Former-commit-id: 8e68a55b0b1a3ce382af33bf0d1a1d073baa7648

--- backend/core/config.py
+++ backend/core/config.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4e5581b7024a2473e7eb68e6150202979cdf772cad65b38f561b17720fe6917e
-size 1813
+oid sha256:a1ca23c81024e4b6f54c7cb60dda31e0a6ed4fe9529feb18442ee592c44118e9
+size 1779
